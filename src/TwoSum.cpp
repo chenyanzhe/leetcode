fa98ec4a25e3324bf7304ad69ef6f8029bfcad8c
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-05-31 13:58:38 +0800

    Problem 1. Two Sum

---
+++ src/TwoSum.cpp
@@ -0,0 +1,22 @@
+#include "TwoSum.hpp"
+
+#include <unordered_map>
+using namespace std;
+
+vector<int> Solution::twoSum(vector<int> &nums, int target)
+{
+  unordered_map<int, int> indexMap;
+  vector<int> ans;
+
+  for (size_t i = 0; i < nums.size(); i++) {
+    int val = nums[i];
+    if (indexMap.find(target - val) != indexMap.end()) {
+      ans.push_back(indexMap[target - val]);
+      ans.push_back((int) i + 1);
+      return ans;
+    } else {
+      indexMap[val] = (int) i + 1;
+    }
+  }
+  return ans;
+}
