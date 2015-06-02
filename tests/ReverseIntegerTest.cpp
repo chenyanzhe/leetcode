2a57b052ab7e39f0f8f2e6a6294fe9b42d2fd304
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-06-03 00:01:59 +0800

    Update Problem 7 test case

--- tests/ReverseIntegerTest.cpp
+++ tests/ReverseIntegerTest.cpp
@@ -9,4 +9,14 @@ TEST_CASE("Reverse Integer") {
     REQUIRE(s.reverse(123) == 321);
     REQUIRE(s.reverse(-123) == -321);
   }
+
+  SECTION("Overflow test") {
+    REQUIRE(s.reverse(-2147483648) == 0);
+    REQUIRE(s.reverse(2147483647) == 0);
+  }
+
+  SECTION("Leading zeros test") {
+    REQUIRE(s.reverse(1234000) == 4321);
+    REQUIRE(s.reverse(-1234000) == -4321);
+  }
 }
