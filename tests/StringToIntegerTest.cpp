479e28ef9df3455c94eeb73013ccb01604ef5d0f
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-06-03 20:28:50 +0800

    Update Problem 8 test case

--- tests/StringToIntegerTest.cpp
+++ tests/StringToIntegerTest.cpp
@@ -7,9 +7,22 @@ TEST_CASE("String To Integer") {
 
   SECTION("Normal tests") {
     REQUIRE(s.myAtoi("1") == 1);
+    REQUIRE(s.myAtoi("378") == 378);
+    REQUIRE(s.myAtoi("-239") == -239);
+    REQUIRE(s.myAtoi("+832") == 832);
   }
 
-  SECTION("Normal tests 2") {
-    REQUIRE(s.myAtoi("    010") == 10);
+  SECTION("Boundary tests") {
+    REQUIRE(s.myAtoi("-2147483648") == -2147483648);
+    REQUIRE(s.myAtoi("-2147483649") == -2147483648);
+    REQUIRE(s.myAtoi("2147483647") == 2147483647);
+    REQUIRE(s.myAtoi("2147483648") == 2147483647);
+  }
+
+  SECTION("Format tests") {
+    REQUIRE(s.myAtoi("asdfasf") == 0);
+    REQUIRE(s.myAtoi("--124") == 0);
+    REQUIRE(s.myAtoi("++4") == 0);
+    REQUIRE(s.myAtoi("    +110?_+120__") == 110);
   }
 }
