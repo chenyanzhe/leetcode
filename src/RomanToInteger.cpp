9b317a6af6de4a7ec919c781b4cee39eaf8dc0a5
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-06-04 20:19:58 +0800

    Problem 13. Roman to Integer

---
+++ src/RomanToInteger.cpp
@@ -0,0 +1,40 @@
+#include "RomanToInteger.hpp"
+
+#include <iostream>
+using namespace std;
+
+int Solution::romanToInt(string s)
+{
+  if (s.empty()) return 0;
+
+  int i;
+  switch(s[0]) {
+  case 'M':
+    i = 0;
+    while (i + 1 < s.size() && s[i + 1] == 'M')
+      i++;
+    return cheatMap[3][s.substr(0, i + 1)] + romanToInt(s.substr(i + 1));
+  case 'C':
+  case 'D':
+    i = 0;
+    while (i + 1 < s.size() &&
+        (s[i + 1] == 'C' || s[i + 1] == 'D' || s[i + 1] == 'M'))
+      i++;
+    return cheatMap[2][s.substr(0, i + 1)] + romanToInt(s.substr(i + 1));
+  case 'X':
+  case 'L':
+    i = 0;
+    while (i + 1 < s.size() &&
+        (s[i + 1] == 'X' || s[i + 1] == 'L' || s[i + 1] == 'C'))
+      i++;
+    return cheatMap[1][s.substr(0, i + 1)] + romanToInt(s.substr(i + 1));
+  case 'I':
+  case 'V':
+    i = 0;
+    while (i + 1 < s.size() &&
+        (s[i + 1] == 'I' || s[i + 1] == 'V' || s[i + 1] == 'X'))
+      i++;
+    return cheatMap[0][s.substr(0, i + 1)] + romanToInt(s.substr(i + 1));
+  }
+  return 0;
+}
---
+++ tests/RomanToIntegerTest.cpp
@@ -0,0 +1,37 @@
+#include "catch.hpp"
+
+#include "RomanToInteger.hpp"
+
+TEST_CASE("Roman To Integer") {
+  Solution s;
+
+  SECTION("Base tests") {
+    REQUIRE(s.romanToInt("I") == 1);
+    REQUIRE(s.romanToInt("II") == 2);
+    REQUIRE(s.romanToInt("III") == 3);
+    REQUIRE(s.romanToInt("XL") == 40);
+    REQUIRE(s.romanToInt("L") == 50);
+    REQUIRE(s.romanToInt("LX") == 60);
+    REQUIRE(s.romanToInt("DCC") == 700);
+    REQUIRE(s.romanToInt("DCCC") == 800);
+    REQUIRE(s.romanToInt("CM") == 900);
+    REQUIRE(s.romanToInt("M") == 1000);
+    REQUIRE(s.romanToInt("MM") == 2000);
+    REQUIRE(s.romanToInt("MMM") == 3000);
+  }
+
+  SECTION("Combination tests") {
+    REQUIRE(s.romanToInt("LXI") == 61);
+    REQUIRE(s.romanToInt("CMLXI") == 961);
+    REQUIRE(s.romanToInt("MMCMLXI") == 2961);
+  }
+
+  SECTION("Zero tests") {
+    REQUIRE(s.romanToInt("CCCXX") == 320);
+    REQUIRE(s.romanToInt("DVII") == 507);
+    REQUIRE(s.romanToInt("MVI") == 1006);
+    REQUIRE(s.romanToInt("MMXXX") == 2030);
+    REQUIRE(s.romanToInt("MMXXX") == 2030);
+    REQUIRE(s.romanToInt("MMMD") == 3500);
+  }
+}
