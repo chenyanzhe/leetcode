b1d2f20b91940a9a2d46a5ccfc44da5a32fcab2e
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-06-18 15:18:50 +0800

    Problem 35. Search Insert Position

---
+++ src/SearchInsertPosition.cpp
@@ -0,0 +1,27 @@
+#include "SearchInsertPosition.hpp"
+
+#include <iostream>
+using namespace std;
+
+int SearchInsertPosition::searchInsert(vector<int>& nums, int target)
+{
+  if (nums.empty()) return 0;
+
+  int l = 0;
+  int r = nums.size() - 1;
+
+  while (l <= r) {
+    int m = (l + r) / 2;
+    if (nums[m] == target)
+      return m;
+    else if (nums[m] < target)
+      l = m + 1;
+    else
+      r = m - 1;
+  }
+
+  if (l == 0) return l;
+  if (r == nums.size() - 1) return r + 1;
+
+  return l;
+}
---
+++ src/SearchInsertPosition.hpp
@@ -0,0 +1,12 @@
+#ifndef SEARCH_INSERT_POSITION_HPP_
+#define SEARCH_INSERT_POSITION_HPP_
+
+#include <vector>
+using namespace std;
+
+class SearchInsertPosition {
+public:
+  int searchInsert(vector<int>& nums, int target);
+};
+
+#endif // SEARCH_INSERT_POSITION_HPP_
