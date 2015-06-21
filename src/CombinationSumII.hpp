405271fbcb6dfd14cf0ad12bf80dddf7fd6774bf
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-06-21 23:33:50 +0800

    Problem 40. Combination Sum II

---
+++ src/CombinationSumII.hpp
@@ -0,0 +1,14 @@
+#ifndef COMBINATION_SUM_II_HPP_
+#define COMBINATION_SUM_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class CombinationSumII {
+public:
+  vector<vector<int>> combinationSum2(vector<int>& candidates, int target);
+private:
+  void helper(vector<int>& candidates, int target, int level, vector<int>& local, vector<vector<int>>& global);
+};
+
+#endif // COMBINATION_SUM_II_HPP_
