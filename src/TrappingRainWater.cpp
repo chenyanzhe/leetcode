0e987635d840a6950ae6d6866a63dbc3503ad9bf
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-06-23 11:17:34 +0800

    Problem 42. Trapping Rain Water

---
+++ src/TrappingRainWater.cpp
@@ -0,0 +1,28 @@
+#include "TrappingRainWater.hpp"
+
+#include <iostream>
+using namespace std;
+
+int TrappingRainWater::trap(vector<int>& height)
+{
+  int width = height.size();
+  if (width == 0) return 0;
+
+  int ret = 0;
+  vector<int> dp(width, 0);
+
+  int max = 0;
+  for (int i = 0; i < width; i++) {
+    dp[i] = max;
+    max = (height[i] > max) ? height[i] : max;
+  }
+
+  max = 0;
+  for (int i = width - 1; i >= 0; i--) {
+    int h = (dp[i] < max) ? dp[i] : max;
+    ret += (h > height[i]) ? h - height[i] : 0;
+    max = (height[i] > max) ? height[i] : max;
+  }
+
+  return ret;
+}
---
+++ src/TrappingRainWater.hpp
@@ -0,0 +1,12 @@
+#ifndef TRAPPING_RAIN_WATER_HPP_
+#define TRAPPING_RAIN_WATER_HPP_
+
+#include <vector>
+using namespace std;
+
+class TrappingRainWater {
+public:
+  int trap(vector<int>& height);
+};
+
+#endif // TRAPPING_RAIN_WATER_HPP_
