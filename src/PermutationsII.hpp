ede731bc342509c1324cf0718d839cf761228051
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-08 10:34:05 +0800

    Problem 47. Permutations II

---
+++ src/PermutationsII.hpp
@@ -0,0 +1,15 @@
+#ifndef PERMUTATIONS_II_HPP_
+#define PERMUTATIONS_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class PermutationsII {
+public:
+  vector<vector<int>> permuteUnique(vector<int>& nums);
+private:
+  void permute(vector<int>& nums, int head, int tail, vector<vector<int>>& ret);
+  bool duplicate(vector<int>& nums, int head, int tail);
+};
+
+#endif // PERMUTATIONS_II_HPP_
