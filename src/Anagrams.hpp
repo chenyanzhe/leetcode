a3dbdbf24e11f8a88f206f7e2db66c29646e914e
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-10 10:59:47 +0800

    Problem 49. Anagrams

---
+++ src/Anagrams.hpp
@@ -0,0 +1,13 @@
+#ifndef ANAGRAMS_HPP_
+#define ANAGRAMS_HPP_
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Anagrams {
+public:
+  vector<string> anagrams(vector<string>& strs);
+};
+
+#endif // ANAGRAMS_HPP_
