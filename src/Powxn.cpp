eeb3afdefd6fbb385428a1483462539cd23f0812
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-11 09:33:06 +0800

    Problem 50. Pow(x, n)

---
+++ src/Powxn.cpp
@@ -0,0 +1,18 @@
+#include "Powxn.hpp"
+
+double Powxn::myPow(double x, int n)
+{
+  return helper(x, (long) n);
+}
+
+double Powxn::helper(double x, long n)
+{
+  if (n == 0) return 1.0;
+  if (n < 0) return helper(1 / x, -n);
+
+  double half = helper(x, n / 2);
+  if (n % 2 == 1)
+    return half * half * x;
+  else
+    return half * half;
+}
---
+++ src/Powxn.hpp
@@ -0,0 +1,11 @@
+#ifndef POWXN_HPP_
+#define POWXN_HPP_
+
+class Powxn {
+public:
+  double myPow(double x, int n);
+private:
+  double helper(double x, long n);
+};
+
+#endif // POWXN_HPP_
