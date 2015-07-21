96839ae65f872eb49c643f89aabf71613925d905
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-21 21:28:37 +0800

    Problem 54. Spiral Matrix problem template

---
+++ src/SpiralMatrix.hpp
@@ -0,0 +1,12 @@
+#ifndef SPIRAL_MATRIX_HPP_
+#define SPIRAL_MATRIX_HPP_
+
+#include <vector>
+using namespace std;
+
+class SpiralMatrix {
+public:
+  vector<int> spiralOrder(vector<vector<int>>& matrix);
+};
+
+#endif // SPIRAL_MATRIX_HPP_
