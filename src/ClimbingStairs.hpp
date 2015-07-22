87436ce9285f6fc7d68047dded543266b94edb1d
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-22 11:33:55 +0800

    Problem 70. Climbing Stairs
    Remove Problem 54. Spiral Matrix problem template

---
+++ src/ClimbingStairs.hpp
@@ -0,0 +1,9 @@
+#ifndef CLIMBING_STAIRS_HPP_
+#define CLIMBING_STAIRS_HPP_
+
+class ClimbingStairs {
+public:
+  int climbStairs(int n);
+};
+
+#endif // CLIMBING_STAIRS_HPP_
---
+++ tests/ClimbingStairsTest.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#include "ClimbingStairs.hpp"
+
+TEST_CASE("Climbing Stairs") {
+  ClimbingStairs s;
+
+  SECTION("Normal tests") {
+    REQUIRE(s.climbStairs(1) == 1);
+    REQUIRE(s.climbStairs(2) == 2);
+    REQUIRE(s.climbStairs(3) == 3);
+  }
+}
