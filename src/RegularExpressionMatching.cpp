957ee26698cb1dae21602edab66d552021fbe071
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-22 15:44:46 +0800

    Problem 10. Regular Expression Matching DP version

--- src/RegularExpressionMatching.cpp
+++ src/RegularExpressionMatching.cpp
@@ -1,5 +1,7 @@
 #include "RegularExpressionMatching.hpp"
 
+#if 0
+// Recursion
 bool RegularExpressionMatching::isMatch(string s, string p)
 {
   if (p.empty()) return s.empty();
@@ -23,4 +25,43 @@ bool RegularExpressionMatching::isMatch(string s, string p)
     return isMatch(s.substr(1), p.substr(1)) && (p[0] == '.' || p[0] == s[0]);
   }
   return false;
+}
+#endif
+
+// Dynamic Programming
+
+#include <vector>
+using namespace std;
+
+bool RegularExpressionMatching::isMatch(string s, string p)
+{
+  int sl = s.size();
+  int pl = p.size();
+
+  vector<vector<bool>> dp(sl + 1, vector<bool>(pl + 1, false));
+
+  // if p == null, then s and p matches iff s == null
+  dp[0][0] = true;
+  for (int i = 1; i <= sl; i++)
+    dp[i][0] = false;
+
+  // if s == null, then s and p matches iff p conforms to (.*)*
+  for (int j = 1; j <= pl; j++)
+    dp[0][j] = j > 1 && p[j - 1] == '*' && dp[0][j - 2];
+
+  for (int i = 1; i <= sl; i++) {
+    for (int j = 1; j <= pl; j++) {
+      if (p[j - 1] == '*')
+        dp[i][j] = dp[i][j - 2] || isMatch(s[i - 1], p[j - 2]) && dp[i - 1][j];
+      else
+        dp[i][j] = dp[i - 1][j - 1] && isMatch(s[i - 1], p[j - 1]);
+    }
+  }
+
+  return dp[sl][pl];
+}
+
+inline bool RegularExpressionMatching::isMatch(char _s, char _p)
+{
+  return _p == '.' || _s == _p;
 }
--- src/RegularExpressionMatching.hpp
+++ src/RegularExpressionMatching.hpp
@@ -7,6 +7,8 @@ using namespace std;
 class RegularExpressionMatching {
 public:
   bool isMatch(string s, string p);
+private:
+  bool isMatch(char _s, char _p);
 };
 
 #endif // REGULAR_EXPRESSION_MATCHING_HPP_
