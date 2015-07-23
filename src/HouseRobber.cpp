c5d79ab37081ed55c7a6960e1fca079ef668cc59
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-23 16:29:31 +0800

    A better solution for Problem 198 House Robber

--- src/HouseRobber.cpp
+++ src/HouseRobber.cpp
@@ -10,10 +10,10 @@ int HouseRobber::rob(vector<int>& nums)
   if (n == 1) return nums[0];
 
   vector<int> dp(n, 0);
-  dp[0] = nums[0]; dp[1] = nums[1];
+  dp[0] = nums[0]; dp[1] = max(nums[0], nums[1]);
 
   for (int i = 2; i < n; i++)
-    dp[i] = *max_element(dp.begin(), dp.begin() + i - 1) + nums[i];
+    dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
 
-  return *max_element(dp.begin(), dp.end());
+  return dp[n - 1];
 }
