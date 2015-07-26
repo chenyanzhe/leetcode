ebbd7246ed892f4c2b8e13dac548a0abfe34037d
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-26 15:08:29 +0800

    Problem 139. Word Break

---
+++ src/WordBreak.cpp
@@ -0,0 +1,24 @@
+#include "WordBreak.hpp"
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool WordBreak::wordBreak(string s, unordered_set<string>& wordDict)
+{
+  int sz = s.size();
+
+  vector<bool> dp(sz + 1, false);
+  dp[0] = true;
+
+  for (int i = 1; i <= sz; i++) {
+    for (int j = 0; j < i; j++) {
+      if (dp[j] && wordDict.find(s.substr(j, i - j)) != wordDict.end()) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+
+  return dp[sz];
+}
---
+++ src/WordBreak.hpp
@@ -0,0 +1,13 @@
+#ifndef WORD_BREAK_HPP_
+#define WORD_BREAK_HPP_
+
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class WordBreak {
+public:
+  bool wordBreak(string s, unordered_set<string>& wordDict);
+};
+
+#endif // WORD_BREAK_HPP_
