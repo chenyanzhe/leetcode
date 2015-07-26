71fa5512dab918f67876e5e535b31834bba73b2c
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-26 10:48:38 +0800

    Problem 63. Unique Paths II

---
+++ src/UniquePathsII.cpp
@@ -0,0 +1,25 @@
+#include "UniquePathsII.hpp"
+
+int UniquePathsII::uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+{
+  int m = obstacleGrid.size();
+  if (m == 0) return 0;
+
+  int n = obstacleGrid[0].size();
+  if (n == 0) return 0;
+
+  vector<vector<int>> dp(m, vector<int>(n, 0));
+  dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+
+  for (int i = 1; i < m; i++)
+    dp[i][0] = (obstacleGrid[i][0] == 1) ? 0 : dp[i-1][0];
+
+  for (int j = 1; j < n; j++)
+    dp[0][j] = (obstacleGrid[0][j] == 1) ? 0 : dp[0][j-1]; 
+
+  for (int i = 1; i < m; i++)
+    for (int j = 1; j < n; j++)
+      dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i-1][j] + dp[i][j-1];
+
+  return dp[m-1][n-1];
+}
---
+++ src/UniquePathsII.hpp
@@ -0,0 +1,12 @@
+#ifndef UNIQUE_PATHS_II_HPP_
+#define UNIQUE_PATHS_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class UniquePathsII {
+public:
+  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid);
+};
+
+#endif // UNIQUE_PATHS_II_HPP_
