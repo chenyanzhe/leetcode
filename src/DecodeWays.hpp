13ad303f7793ca7fc19830f607fe475a8d0986ef
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-28 14:53:47 +0800

    Problem 91. Decode Ways

---
+++ src/DecodeWays.hpp
@@ -0,0 +1,14 @@
+#ifndef DECODE_WAYS_HPP_
+#define DECODE_WAYS_HPP_
+
+#include <string>
+using namespace std;
+
+class DecodeWays {
+public:
+  int numDecodings(string s);
+private:
+  bool valid(string s);
+};
+
+#endif // DECODE_WAYS_HPP_
---
+++ tests/DecodeWaysTest.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+#include "DecodeWays.hpp"
+
+TEST_CASE("Decode Ways") {
+  DecodeWays s;
+
+  SECTION("Sample test") {
+    REQUIRE(s.numDecodings("12") == 2);
+  }
+
+  SECTION("Normal tests") {
+    REQUIRE(s.numDecodings("01") == 0);
+    REQUIRE(s.numDecodings("10") == 1);
+    REQUIRE(s.numDecodings("11") == 2);
+    REQUIRE(s.numDecodings("26") == 2);
+    REQUIRE(s.numDecodings("27") == 1);
+  }
+}
