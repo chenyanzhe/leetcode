1788003fce0d885b591499c1e56ae2f4c9df6ea4
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-07-29 14:45:28 +0800

    Problem 120. Triangle

---
+++ src/Triangle.hpp
@@ -0,0 +1,12 @@
+#ifndef TRIANGLE_HPP_
+#define TRIANGLE_HPP_
+
+#include <vector>
+using namespace std;
+
+class Triangle {
+public:
+  int minimumTotal(vector<vector<int>>& triangle);
+};
+
+#endif // TRIANGLE_HPP_
---
+++ tests/TriangleTest.cpp
@@ -0,0 +1,17 @@
+#include "catch.hpp"
+
+#include "Triangle.hpp"
+
+TEST_CASE("Triangle") {
+  Triangle s;
+
+  SECTION("Sample test") {
+    vector<vector<int>> triangle {
+      {2},
+      {3, 4},
+      {6, 5, 7},
+      {4, 1, 8, 3}
+    };
+    REQUIRE(s.minimumTotal(triangle) == 11);
+  }
+}
