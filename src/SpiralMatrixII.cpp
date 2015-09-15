2a722381ef3a4806eabe41b8501201979eeaf42d
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-09-15 14:09:10 +0800

    Problem 59. Spiral Matrix II

---
+++ src/SpiralMatrixII.cpp
@@ -0,0 +1,32 @@
+#include "SpiralMatrixII.hpp"
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> SpiralMatrixII::generateMatrix(int n) {
+	vector<vector<int>> ret(n, vector<int>(n, 0));
+
+	int x = 0;
+	int y = 0;
+	int c = 1;
+
+	while (n > 0) {
+		if (n == 1) {
+			ret[x][y] = c;
+			break;
+		} else {
+			for (int i = 0; i < n - 1; i++)
+				ret[x][y++] = c++;
+			for (int i = 0; i < n - 1; i++)
+				ret[x++][y] = c++;
+			for (int i = 0; i < n - 1; i++)
+				ret[x][y--] = c++;
+			for (int i = 0; i < n - 1; i++)
+				ret[x--][y] = c++;
+		}
+		x++;
+		y++;
+		n -= 2;
+	}
+
+	return ret;
+}
---
+++ src/SpiralMatrixII.hpp
@@ -0,0 +1,12 @@
+#ifndef SPIRAL_MATRIX_II_HPP_
+#define SPIRAL_MATRIX_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class SpiralMatrixII {
+public:
+	vector<vector<int>> generateMatrix(int n);
+};
+
+#endif // SPIRAL_MATRIX_II_HPP_
