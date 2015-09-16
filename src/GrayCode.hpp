ad4f7051540b88faf14b3a8ed8a4481a4401ba43
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-09-16 22:19:06 +0800

    Problem 89. Gray Code

---
+++ src/GrayCode.hpp
@@ -0,0 +1,14 @@
+#ifndef GRAY_CODE_HPP_
+#define GRAY_CODE_HPP_
+
+#include <vector>
+using namespace std;
+
+class GrayCode {
+public:
+	vector<int> grayCode(int n);
+private:
+	void helper(int n, vector<int>& res);
+};
+
+#endif // GRAY_CODE_HPP_
