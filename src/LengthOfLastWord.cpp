896a200c1ca5c8f0de36b2445e8f200db53925f4
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-09-21 17:24:38 +0800

    Problem 58. Length of Last Word

---
+++ src/LengthOfLastWord.cpp
@@ -0,0 +1,18 @@
+#include "LengthOfLastWord.hpp"
+
+int LengthOfLastWord::lengthOfLastWord(string s)
+{
+	bool inWord = false;
+	int len = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ' && inWord) {
+			inWord = false;
+		} else if (s[i] != ' ') {
+			len = inWord ? len + 1 : 1;
+			inWord = true;
+		}
+	}
+
+	return len;
+}
---
+++ src/LengthOfLastWord.hpp
@@ -0,0 +1,12 @@
+#ifndef LENGTH_OF_LAST_WORD_HPP_
+#define LENGTH_OF_LAST_WORD_HPP_
+
+#include <string>
+using namespace std;
+
+class LengthOfLastWord {
+public:
+	int lengthOfLastWord(string s);
+};
+
+#endif // LENGTH_OF_LAST_WORD_HPP_
