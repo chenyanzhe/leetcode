5584d08a70ba97dad0747993f79d8aa29d06703b
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-09-23 22:09:17 +0800

    Problem 66. Plus One

---
+++ src/PlusOne.hpp
@@ -0,0 +1,12 @@
+#ifndef PLUS_ONE_HPP_
+#define PLUS_ONE_HPP_
+
+#include <vector>
+using namespace std;
+
+class PlusOne {
+public:
+	vector<int> plusOne(vector<int>& digits);
+};
+
+#endif // PLUS_ONE_HPP_
