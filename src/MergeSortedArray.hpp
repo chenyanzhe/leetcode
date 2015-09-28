4e6967d04f3339b5ab7a36e04fafbbd5bad8d4ff
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-09-28 17:13:22 +0800

    Problem 88. Merge Sorted Array

---
+++ src/MergeSortedArray.hpp
@@ -0,0 +1,12 @@
+#ifndef MERGE_SORTED_ARRAY_HPP_
+#define MERGE_SORTED_ARRAY_HPP_
+
+#include <vector>
+using namespace std;
+
+class MergeSortedArray {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n);
+};
+
+#endif // MERGE_SORTED_ARRAY_HPP_
