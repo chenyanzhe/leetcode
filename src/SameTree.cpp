6b2ba81fabdb2776ea04a72b500deb955bf62a8a
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-09-29 10:18:51 +0800

    Problem 100. Same Tree

---
+++ src/SameTree.cpp
@@ -0,0 +1,11 @@
+#include "SameTree.hpp"
+
+bool SameTree::isSameTree(TreeNode* p, TreeNode* q)
+{
+	if (p == nullptr && q == nullptr)
+		return true;
+	if (p == nullptr || q == nullptr)
+		return false;
+
+	return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
---
+++ src/SameTree.hpp
@@ -0,0 +1,11 @@
+#ifndef SAME_TREE_HPP_
+#define SAME_TREE_HPP_
+
+#include "TreeNode.hpp"
+
+class SameTree {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q);
+};
+
+#endif // SAME_TREE_HPP_
