00e16e92333068061428b8bededfd41ba048c6c8
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-07 20:29:27 +0800

    Problem 92. Reverse Linked List II

---
+++ src/ReverseLinkedListII.hpp
@@ -0,0 +1,11 @@
+#ifndef REVERSE_LINKED_LIST_II_HPP_
+#define REVERSE_LINKED_LIST_II_HPP_
+
+#include "ListNode.hpp"
+
+class ReverseLinkedListII {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n);
+};
+
+#endif // REVERSE_LINKED_LIST_II_HPP_
