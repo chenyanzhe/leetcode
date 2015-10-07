58275849ad8bf8ad0e7f7b1c07990990d5079729
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-07 21:50:02 +0800

    Problem 97. Interleaving String

---
+++ src/InterleavingString.hpp
@@ -0,0 +1,12 @@
+#ifndef INTERLEAVING_STRING_HPP_
+#define INTERLEAVING_STRING_HPP_
+
+#include <string>
+using namespace std;
+
+class InterleavingString {
+public:
+	bool isInterleave(string s1, string s2, string s3);
+};
+
+#endif // INTERLEAVING_STRING_HPP_
---
+++ tests/InterleavingStringTest.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+
+#include "InterleavingString.hpp"
+
+TEST_CASE("Interleaving String") {
+  	InterleavingString s;
+
+	SECTION("Sample tests") {
+		REQUIRE(s.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+		REQUIRE_FALSE(s.isInterleave("aabcc", "dbbca", "aadbbbaccc"));
+	}
+}
