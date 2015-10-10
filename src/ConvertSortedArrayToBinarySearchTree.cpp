e435dad540dec3665d3a998f69f8d7f6f64ec198
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-10 14:24:46 +0800

    Problem 108. Convert Sorted Array to Binary Search Tree

---
+++ src/ConvertSortedArrayToBinarySearchTree.cpp
@@ -0,0 +1,22 @@
+#include "ConvertSortedArrayToBinarySearchTree.hpp"
+
+TreeNode* ConvertSortedArrayToBinarySearchTree::sortedArrayToBST(vector<int>& nums)
+{
+	return sortedArrayTOBST(nums, 0, nums.size() - 1);
+}
+
+TreeNode* ConvertSortedArrayToBinarySearchTree::sortedArrayTOBST(vector<int>& nums, int begin, int end)
+{
+	if (begin > end) return nullptr;
+
+	int mid = (begin + end) / 2;
+
+	TreeNode *root = new TreeNode(nums[mid]);
+
+	if (begin == end) return root;
+
+	root->left = sortedArrayTOBST(nums, begin, mid - 1);
+	root->right = sortedArrayTOBST(nums, mid + 1, end);
+
+	return root;
+}
---
+++ src/ConvertSortedArrayToBinarySearchTree.hpp
@@ -0,0 +1,16 @@
+#ifndef CONVERT_SORTED_ARRAY_TO_BINARY_SEARCH_TREE_HPP_
+#define CONVERT_SORTED_ARRAY_TO_BINARY_SEARCH_TREE_HPP_
+
+#include "TreeNode.hpp"
+
+#include <vector>
+using namespace std;
+
+class ConvertSortedArrayToBinarySearchTree {
+public:
+	TreeNode* sortedArrayToBST(vector<int>& nums);
+private:
+	TreeNode* sortedArrayTOBST(vector<int>& nums, int begin, int end);
+};
+
+#endif // CONVERT_SORTED_ARRAY_TO_BINARY_SEARCH_TREE_HPP_
