d79fae61501ba522507acdb89b91cd888c90e56e
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-12 15:28:45 +0800

    Problem 113. Path Sum II

---
+++ src/PathSumII.hpp
@@ -0,0 +1,18 @@
+#ifndef PATH_SUM_II_HPP_
+#define PATH_SUM_II_HPP_
+
+#include "TreeNode.hpp"
+
+#include <vector>
+using namespace std;
+
+class PathSumII {
+private:
+	vector<vector<int>> res;
+public:
+	vector<vector<int>> pathSum(TreeNode* root, int sum);
+private:
+	void pathSum(TreeNode *root, vector<int> &path, int sum);
+};
+
+#endif // PATH_SUM_II_HPP_
