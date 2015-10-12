cc3b5088f9ef1620655dc3900f5e2bbfa0880f30
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-12 14:37:05 +0800

    Problem 111. Minimum Depth of Binary Tree

---
+++ src/MinimumDepthOfBinaryTree.hpp
@@ -0,0 +1,11 @@
+#ifndef MINIMUM_DEPTH_OF_BINARY_TREE_HPP_
+#define MINIMUM_DEPTH_OF_BINARY_TREE_HPP_
+
+#include "TreeNode.hpp"
+
+class MinimumDepthOfBinaryTree {
+public:
+	int minDepth(TreeNode* root);
+};
+
+#endif // MINIMUM_DEPTH_OF_BINARY_TREE_HPP_
