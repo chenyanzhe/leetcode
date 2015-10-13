63f8abb9c382a2cbcea1e0294746a37fc86a293e
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-13 16:01:24 +0800

    Problem 117. Populating Next Right Pointers in Each Node II

---
+++ src/PopulatingNextRightPointersInEachNodeII.cpp
@@ -0,0 +1,23 @@
+#include "PopulatingNextRightPointersInEachNodeII.hpp"
+
+#include <queue>
+using namespace std;
+
+void PopulatingNextRightPointersInEachNodeII::connect(TreeLinkNode *root)
+{
+	if (root == nullptr) return;
+
+	queue<TreeLinkNode *> q;
+	q.push(root);
+
+	while (!q.empty()) {
+		int k = q.size(); // number of nodes in this level
+		for (int i = 0; i < k; i++) {
+			TreeLinkNode *front = q.front();
+			q.pop();
+			if (i + 1 < k) front->next = q.front();
+			if (front->left != nullptr) q.push(front->left);
+			if (front->right != nullptr) q.push(front->right);
+		}
+	}
+}
