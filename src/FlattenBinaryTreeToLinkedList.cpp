13f4ad29840bd894164afdf9cc38a257e712994f
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-13 15:18:04 +0800

    Problem 114. Flatten Binary Tree to Linked List

---
+++ src/FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,26 @@
+#include "FlattenBinaryTreeToLinkedList.hpp"
+
+void FlattenBinaryTreeToLinkedList::flatten(TreeNode* root)
+{
+	if (root == nullptr) return;
+
+	// flatten left subtree
+	if (root->left != nullptr) flatten(root->left);
+	// flatten right subtree
+	if (root->right != nullptr) flatten(root->right);
+
+	if (root->left != nullptr) {
+		// make left subtree as the right node of root
+		// and empty root's left node
+		TreeNode *rt = root->right;
+		root->right = root->left;
+		root->left = nullptr;
+
+		// 1. find the tail node of the first list (head at root)
+		TreeNode *tail = root->right;
+		while (tail->right != nullptr)
+			tail = tail->right;
+		// 2. link two lists (head at root and rt) together
+		tail->right = rt;
+	}
+}
---
+++ src/FlattenBinaryTreeToLinkedList.hpp
@@ -0,0 +1,11 @@
+#ifndef FLATTEN_BINARY_TREE_TO_LINKED_LIST_HPP_
+#define FLATTEN_BINARY_TREE_TO_LINKED_LIST_HPP_
+
+#include "TreeNode.hpp"
+
+class FlattenBinaryTreeToLinkedList {
+public:
+	void flatten(TreeNode* root);
+};
+
+#endif // FLATTEN_BINARY_TREE_TO_LINKED_LIST_HPP_
