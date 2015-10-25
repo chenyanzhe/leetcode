bc542a2691e9ad02aea2e1408db6a303f9ded4bc
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-25 20:12:10 +0800

    Refine solution.

--- src/MedianOfTwoSortedArrays.cpp
+++ src/MedianOfTwoSortedArrays.cpp
@@ -3,30 +3,41 @@
 #include <algorithm>
 using namespace std;
 
-double MedianOfTwoSortedArrays::findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+double MedianOfTwoSortedArrays::findMedianSortedArrays(vector<int>& A, vector<int>& B)
 {
-    int m = nums1.size(), n = nums2.size(), mid = (m + n) / 2;
-    bool even = ((m + n) & 1) == 0;
+    if (A.empty() && B.empty())
+        return 0;
 
-    if (m == 0 && n == 0) return 0;
+    return findMedianSortedArrays(A.data(), A.size(), B.data(), B.size());
+}
+
+double MedianOfTwoSortedArrays::findMedianSortedArrays(int A[], int m, int B[], int n)
+{
+    int total = m + n;
+    if (total & 1)
+        return findKth(A, m, B, n, total / 2 + 1);
+    else
+        return (findKth(A, m, B, n, total / 2) + findKth(A, m, B, n, total / 2 + 1)) / 2;
+}
+
+double MedianOfTwoSortedArrays::findKth(int A[], int m, int B[], int n, int k)
+{
+    if (m > n)
+        return findKth(B, n, A, m, k);
+
     if (m == 0)
-        return !even ? nums2[mid]: (nums2[mid] + nums2[mid - 1]) / 2.0;
-    if (n == 0)
-        return !even ? nums1[mid]: (nums1[mid] + nums1[mid - 1]) / 2.0;
+        return B[k-1];
 
-    int left = max(0, mid - n), right = min(mid, m - 1);
+    if (k == 1)
+        return min(A[0], B[0]);
 
-    while (left <= right) {
-        int i = (left + right) / 2, j = mid - 1 - i;
-        if (j >= 0 && nums1[i] < nums2[j]) left = i + 1;
-        else if (j < n - 1 && nums1[i] > nums2[j+1]) right = i - 1;
-        else {
-            if (!even) return nums1[i];
-            else if (j < 0) return (nums1[i-1] + nums1[i]) / 2.0;
-            else if (i <= 0) return (nums2[j] + nums1[i]) / 2.0;
-            else return (max(nums2[j], nums1[i-1]) + nums1[i]) / 2.0;
-        }
-    }
-
-    return findMedianSortedArrays(nums2, nums1);
+    int i = min(k / 2, m);
+    int j = k - i;
+    
+    if (A[i - 1] < B[j - 1])
+        return findKth(A + i, m - i, B, n, k - i);
+    else if (A[i - 1] > B[j - 1])
+        return findKth(A, m, B + j, n - j, k - j);
+    else
+        return A[i - 1];
 }
--- src/MedianOfTwoSortedArrays.hpp
+++ src/MedianOfTwoSortedArrays.hpp
@@ -7,6 +7,9 @@ using namespace std;
 class MedianOfTwoSortedArrays {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
+private:
+    double findMedianSortedArrays(int nums1[], int sz1, int nums2[], int sz2);
+    double findKth(int nums1[], int sz1,  int nums2[], int sz2, int k);
 };
 
 #endif // MEDIAN_OF_TWO_SORTED_ARRAYS_HPP_
