1224274671f2de1bc6ddc664daa04fed2343c9b0
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-10-25 20:56:47 +0800

    Refine solution.

--- src/TwoSum.cpp
+++ src/TwoSum.cpp
@@ -8,18 +8,16 @@ vector<int> TwoSum::twoSum(vector<int> &nums, int target)
     unordered_map<int, int> mapping;
     vector<int> result;
 
-    // preprocess
-    for (int i = 0; i < nums.size(); i++)
-        mapping[nums[i]] = i;
-
     for (int i = 0; i < nums.size(); i++) {
         int gap = target - nums[i];
-        if (mapping.find(gap) != mapping.end() && mapping[gap] > i) {
-            result.push_back(i + 1);
+        if (mapping.find(gap) != mapping.end()) {
             result.push_back(mapping[gap] + 1);
+            result.push_back(i + 1);
             break;
+        } else {
+            mapping[nums[i]] = i;
         }
     }
-    
+
     return result;
 }
