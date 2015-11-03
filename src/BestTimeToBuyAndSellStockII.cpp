d4f038fb0cf1dec0c827969b4a0d3451e5ac5418
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-03 20:30:35 +0800

    Problem 122. Best Time to Buy and Sell Stock II

---
+++ src/BestTimeToBuyAndSellStockII.cpp
@@ -0,0 +1,17 @@
+#include "BestTimeToBuyAndSellStockII.hpp"
+
+int BestTimeToBuyAndSellStockII::maxProfit(vector<int>& prices)
+{
+	int sz = prices.size();
+
+	if (sz <= 1) return 0;
+
+	int result = 0;
+
+	for (int i = 1; i < sz; i++) {
+		int diff = prices[i] - prices[i - 1];
+		if (diff > 0) result += diff;
+	}
+
+	return result;
+}
