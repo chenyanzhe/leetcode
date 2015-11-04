e3bef54280bf1eed8273c11d7da99fe50660e9f6
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-04 17:26:23 +0800

    Problem 219. Contains Duplicate II

---
+++ src/ContainsDuplicateII.hpp
@@ -0,0 +1,12 @@
+#ifndef CONTAINS_DUPLICATE_II_HPP_
+#define CONTAINS_DUPLICATE_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class ContainsDuplicateII {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k);
+};
+
+#endif // CONTAINS_DUPLICATE_II_HPP_
---
+++ tests/ContainsDuplicateIITest.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#include "ContainsDuplicateII.hpp"
+
+TEST_CASE("Contains Duplicate II") {
+    ContainsDuplicateII s;
+
+    SECTION("Sample test") {
+        vector<int> nums {1, 0, -1, 0, -2, 2};
+        REQUIRE_FALSE(s.containsNearbyDuplicate(nums, 1));
+        REQUIRE(s.containsNearbyDuplicate(nums, 2));
+    }
+}
