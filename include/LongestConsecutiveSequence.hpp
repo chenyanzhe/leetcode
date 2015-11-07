e2f6f21eb2a21bab358fdf197a1d72284a24643f
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-07 21:46:13 +0800

    Problem 128. Longest Consecutive Sequence

---
+++ include/LongestConsecutiveSequence.hpp
@@ -0,0 +1,12 @@
+#ifndef LONGEST_CONSECUTIVE_SEQUENCE_HPP_
+#define LONGEST_CONSECUTIVE_SEQUENCE_HPP_
+
+#include <vector>
+using namespace std;
+
+class LongestConsecutiveSequence {
+public:
+	int longestConsecutive(vector<int>& nums);
+};
+
+#endif // LONGEST_CONSECUTIVE_SEQUENCE_HPP_
---
+++ tests/LongestConsecutiveSequenceTest.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+
+#include "LongestConsecutiveSequence.hpp"
+
+TEST_CASE("Longest Consecutive Sequence") {
+    LongestConsecutiveSequence s;
+
+    SECTION("Sample test") {
+        vector<int> nums {100, 4, 200, 1, 3, 2};
+        REQUIRE(s.longestConsecutive(nums) == 4);
+    }
+}
