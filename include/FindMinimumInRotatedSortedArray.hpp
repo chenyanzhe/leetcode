efcdbac4c5617f3d62ca9381db4bd79d7d1df8dd
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-09 19:20:16 +0800

    Problem 153. Find Minimum in Rotated Sorted Array

---
+++ include/FindMinimumInRotatedSortedArray.hpp
@@ -0,0 +1,14 @@
+#ifndef FIND_MINIMUM_IN_ROTATED_SORTED_ARRAY_HPP_
+#define FIND_MINIMUM_IN_ROTATED_SORTED_ARRAY_HPP_
+
+#include <vector>
+using namespace std;
+
+class FindMinimumInRotatedSortedArray {
+public:
+    int findMin(vector<int>& nums);
+private:
+    int findMin(vector<int>& nums, int begin, int end);
+};
+
+#endif // FIND_MINIMUM_IN_ROTATED_SORTED_ARRAY_HPP_
