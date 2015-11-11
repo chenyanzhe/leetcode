8d2ab962b09cdd3d742c0201f4c095f6a88bbeca
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-11 10:31:31 +0800

    Problem 165. Compare Version Numbers

---
+++ include/CompareVersionNumbers.hpp
@@ -0,0 +1,16 @@
+#ifndef COMPARE_VERSION_NUMBERS_HPP_
+#define COMPARE_VERSION_NUMBERS_HPP_
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class CompareVersionNumbers {
+public:
+    int compareVersion(string version1, string version2);
+private:
+    int compareDigitsString(string num1, string num2);
+    void tokenize(const string &version, vector<string> &versions);
+};
+
+#endif // COMPARE_VERSION_NUMBERS_HPP_
