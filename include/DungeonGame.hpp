22df81820f72513dc307c32b60d1bfc4ccf09ab1
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-15 17:57:38 +0800

    Problem 174. Dungeon Game

---
+++ include/DungeonGame.hpp
@@ -0,0 +1,12 @@
+#ifndef DUNGEON_GAME_HPP_
+#define DUNGEON_GAME_HPP_
+
+#include <vector>
+using namespace std;
+
+class DungeonGame {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon);
+};
+
+#endif // DUNGEON_GAME_HPP_
---
+++ tests/DungeonGameTest.cpp
@@ -0,0 +1,16 @@
+#include "catch.hpp"
+
+#include "DungeonGame.hpp"
+
+TEST_CASE("Dungeon Game") {
+    DungeonGame s;
+
+    SECTION("Sample test") {
+        vector<vector<int>> dungeon {
+            {-2, -3, 3},
+            {-5, -10, 1},
+            {10, 30, -5}
+        };
+        REQUIRE(s.calculateMinimumHP(dungeon) == 7);
+    }
+}
