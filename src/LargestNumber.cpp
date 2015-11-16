e173a6abe073792f32d49aa94a9f16e387830c6b
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-16 22:57:27 +0800

    Problem 179. Largest Number

--- src/LargestNumber.cpp
+++ src/LargestNumber.cpp
@@ -22,36 +22,34 @@ string LargestNumber::largestNumber(vector<string>& nums)
 
     function<bool (const string&, const string&)> comp;
     comp = [&comp](const string& a, const string& b) {
-        // int sza = a.size();
-        // int szb = b.size();
+        int sza = a.size();
+        int szb = b.size();
 
-        // if (sza > szb) return comp(b, a);
+        if (sza > szb) return !comp(b, a);
 
-        // // len(a) <= len(b)
-        // // [--a--][----b----]
-        // // [----b----][--a--]
-        // // a[0 : sza - 1] v.s. b[0 : sza - 1]
-        // // b[0 : szb - sza - 1] v.s. b[sza : szb - 1]
-        // // b[szb - sza : szb - 1] v.s. a[0 : sza - 1]
+        // len(a) <= len(b)
+        // [--a--][----b----]
+        // [----b----][--a--]
+        // a[0 : sza - 1] v.s. b[0 : sza - 1]
+        // b[0 : szb - sza - 1] v.s. b[sza : szb - 1]
+        // b[szb - sza : szb - 1] v.s. a[0 : sza - 1]
 
-        // for (int i = 0, j = 0; i < sza && j < sza; i++, j++) {
-        //  if (a[i] > b[j]) return true;
-        //  if (a[i] < b[j]) return false;
-        // }
+        for (int i = 0, j = 0; i < sza && j < sza; i++, j++) {
+            if (a[i] > b[j]) return true;
+            if (a[i] < b[j]) return false;
+        }
 
-        // for (int i = 0, j = sza; i < szb - sza && j < szb; i++, j++) {
-        //  if (b[i] > b[j]) return true;
-        //  if (b[i] < b[j]) return false;
-        // }
+        for (int i = 0, j = sza; i < szb - sza && j < szb; i++, j++) {
+            if (b[i] > b[j]) return true;
+            if (b[i] < b[j]) return false;
+        }
 
-        // for (int i = szb - sza, j = 0; i < szb && j < sza; i++, j++) {
-        //  if (b[i] > a[j]) return true;
-        //  if (b[i] < a[j]) return false;
-        // }
+        for (int i = szb - sza, j = 0; i < szb && j < sza; i++, j++) {
+            if (b[i] > a[j]) return true;
+            if (b[i] < a[j]) return false;
+        }
 
-        // return false;
-
-        return a + b > b + a;
+        return false;
     };
 
     sort(nums.begin(), nums.end(), comp);
