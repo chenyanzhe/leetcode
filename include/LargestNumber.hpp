6548cf36df4f315f0806638d8b10f0d7002c8f31
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-16 22:03:36 +0800

    Problem 179. Largest Number

---
+++ include/LargestNumber.hpp
@@ -0,0 +1,15 @@
+#ifndef LARGEST_NUMBER_HPP_
+#define LARGEST_NUMBER_HPP_
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class LargestNumber {
+public:
+    string largestNumber(vector<int>& nums);
+private:
+    string largestNumber(vector<string>& nums);
+};
+
+#endif // LARGEST_NUMBER_HPP_
---
+++ tests/LargestNumberTest.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#include "LargestNumber.hpp"
+
+TEST_CASE("Largest Number") {
+    LargestNumber s;
+
+    SECTION("Sample test") {
+        vector<int> nums {3, 30, 34, 5, 9};
+
+        REQUIRE(s.largestNumber(nums) == "9534330");
+    }
+}
