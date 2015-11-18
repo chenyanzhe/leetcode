424f16e5951fcfcaf779ccda2e10679a8322e8dd
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-18 23:37:00 +0800

    Problem 190. Reverse Bits

---
+++ include/ReverseBits.hpp
@@ -0,0 +1,12 @@
+#ifndef REVERSE_BITS_HPP_
+#define REVERSE_BITS_HPP_
+
+#include <cstdint>
+using namespace std;
+
+class ReverseBits {
+public:
+    uint32_t reverseBits(uint32_t n);
+};
+
+#endif // REVERSE_BITS_HPP_
---
+++ tests/ReverseBitsTest.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+
+#include "ReverseBits.hpp"
+
+TEST_CASE("Reverse Bits") {
+    ReverseBits s;
+
+    SECTION("Sample test") {
+        uint32_t input = 43261596;
+        uint32_t expected = 964176192;
+        REQUIRE(s.reverseBits(input) == expected);
+    }
+}
