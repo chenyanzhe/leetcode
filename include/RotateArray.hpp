a0f30c099fa0390166485b09a625302fd9b94404
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-18 23:13:45 +0800

    Problem 189. Rotate Array

---
+++ include/RotateArray.hpp
@@ -0,0 +1,12 @@
+#ifndef ROTATE_ARRAY_HPP_
+#define ROTATE_ARRAY_HPP_
+
+#include <vector>
+using namespace std;
+
+class RotateArray {
+public:
+    void rotate(vector<int>& nums, int k);
+};
+
+#endif // ROTATE_ARRAY_HPP_
