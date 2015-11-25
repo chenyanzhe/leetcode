236dfc77a9de61075481ec5c1c73a065eff3c4a1
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-25 22:44:40 +0800

    Problem 201. Bitwise AND of Numbers Range

---
+++ include/BitwiseANDOfNumbersRange.hpp
@@ -0,0 +1,9 @@
+#ifndef BITWISE_AND_OF_NUMBERS_RANGE_HPP_
+#define BITWISE_AND_OF_NUMBERS_RANGE_HPP_
+
+class BitwiseANDOfNumbersRange {
+public:
+    int rangeBitwiseAnd(int m, int n);
+};
+
+#endif // BITWISE_AND_OF_NUMBERS_RANGE_HPP_
---
+++ tests/BitwiseANDOfNumbersRangeTest.cpp
@@ -0,0 +1,11 @@
+#include "catch.hpp"
+
+#include "BitwiseANDOfNumbersRange.hpp"
+
+TEST_CASE("Bitwise AND of Numbers Range") {
+    BitwiseANDOfNumbersRange s;
+
+    SECTION("Sample test") {
+        REQUIRE(s.rangeBitwiseAnd(5, 7) == 4);
+    }
+}
