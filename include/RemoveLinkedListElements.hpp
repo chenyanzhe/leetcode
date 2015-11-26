f23a9eeb8cbd5b93b402783c09fd98b551e61bb2
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-26 19:08:16 +0800

    Problem 203. Remove Linked List Elements

---
+++ include/RemoveLinkedListElements.hpp
@@ -0,0 +1,11 @@
+#ifndef REMOVE_LINKED_LIST_ELEMENTS_HPP_
+#define REMOVE_LINKED_LIST_ELEMENTS_HPP_
+
+#include "ListNode.hpp"
+
+class RemoveLinkedListElements {
+public:
+    ListNode* removeElements(ListNode* head, int val);
+};
+
+#endif // REMOVE_LINKED_LIST_ELEMENTS_HPP_
---
+++ src/RemoveLinkedListElements.cpp
@@ -0,0 +1,22 @@
+#include "RemoveLinkedListElements.hpp"
+
+ListNode* RemoveLinkedListElements::removeElements(ListNode* head, int val)
+{
+    while (head != nullptr && head->val == val)
+        head = head->next;
+    if (head == nullptr) return nullptr;
+    
+    ListNode *p = head;
+    ListNode *q = head->next;
+    while (q != nullptr) {
+        if (q->val == val) {
+            p->next = q->next;
+            q = q->next;
+        } else {
+            p = q;
+            q = q->next;
+        }
+    }
+    
+    return head;
+}
