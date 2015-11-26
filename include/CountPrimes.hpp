3284a638b67a2315e0e995c110c55ab8eef2a8b4
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-11-26 22:28:28 +0800

    Problem 204. Count Primes

---
+++ include/CountPrimes.hpp
@@ -0,0 +1,9 @@
+#ifndef COUNT_PRIMES_HPP_
+#define COUNT_PRIMES_HPP_
+
+class CountPrimes {
+public:
+    int countPrimes(int n);
+};
+
+#endif // COUNT_PRIMES_HPP_
---
+++ src/CountPrimes.cpp
@@ -0,0 +1,27 @@
+#include "CountPrimes.hpp"
+
+#include <vector>
+using namespace std;
+
+int CountPrimes::countPrimes(int n)
+{
+    int result = 0;
+    vector<bool> notPrime(n, false);
+    notPrime[1] = true;
+    
+    int base = 2;
+    while (base * base < n) {
+        // marking
+        for (int i = base; i * base < n; i++)
+            notPrime[i * base] = true;
+        // update new base
+        int i = base + 1;
+        while (i < n && notPrime[i])
+            i++;
+        base = i;
+    }
+    
+    for (int i = 1; i < n; i++)
+        if (!notPrime[i]) result++;
+    return result;
+}
