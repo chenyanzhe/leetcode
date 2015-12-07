ed3642b02b20c636dcb0a47064d16c4f158ad001
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-07 20:19:13 +0800

    Problem 212. Word Search II

---
+++ include/WordSearchII.hpp
@@ -0,0 +1,22 @@
+#ifndef WORD_SEARCH_II_HPP_
+#define WORD_SEARCH_II_HPP_
+
+#include "ImplementTrie.hpp"
+
+#include <vector>
+#include <string>
+#include <set>
+using namespace std;
+
+class WordSearchII {
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words);
+private:
+    Trie trie;
+    void findWords(const vector<vector<char>>& board,
+                  vector<vector<bool>>& visited,
+                  int row, int col, string word,
+                  set<string>& result);
+};
+
+#endif // WORD_SEARCH_II_HPP_
