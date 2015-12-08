1610621ca2f41bf4a9e5e04f886906cb865b5fbb
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-08 21:47:27 +0800

    Problem 214. Shortest Palindrome

---
+++ include/ShortestPalindrome.hpp
@@ -0,0 +1,12 @@
+#ifndef SHORTEST_PALINDROME_HPP_
+#define SHORTEST_PALINDROME_HPP_
+
+#include <string>
+using namespace std;
+
+class ShortestPalindrome {
+public:
+    string shortestPalindrome(string s);
+};
+
+#endif // SHORTEST_PALINDROME_HPP_
---
+++ tests/ShortestPalindromeTest.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+
+#include "ShortestPalindrome.hpp"
+
+TEST_CASE("Shortest Palindrome") {
+    ShortestPalindrome s;
+
+    SECTION("Sample test") {
+        REQUIRE(s.shortestPalindrome("aacecaaa") == "aaacecaaa");
+        REQUIRE(s.shortestPalindrome("abcd") == "dcbabcd");
+    }
+}
