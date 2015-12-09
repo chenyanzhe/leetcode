2f80a93ac15f6e0c7d02549be3db0f493f21d696
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-09 23:56:53 +0800

    Problem 216. Combination Sum III

---
+++ include/CombinationSumIII.hpp
@@ -0,0 +1,14 @@
+#ifndef COMBINATION_SUM_III_HPP_
+#define COMBINATION_SUM_III_HPP_
+
+#include <vector>
+using namespace std;
+
+class CombinationSumIII {
+public:
+    vector<vector<int>> combinationSum3(int k, int n);
+private:
+    void bt(int k, int n, int i, vector<int>& now, vector<vector<int>>& result);
+};
+
+#endif // COMBINATION_SUM_III_HPP_
---
+++ src/CombinationSumIII.cpp
@@ -0,0 +1,22 @@
+#include "CombinationSumIII.hpp"
+
+vector<vector<int>> CombinationSumIII::combinationSum3(int k, int n)
+{
+    vector<vector<int>> result;
+    vector<int> now;
+    bt(k, n, 1, now, result);
+    return result;
+}
+
+void CombinationSumIII::bt(int k, int n, int i, vector<int>& now, vector<vector<int>>& result)
+{
+    if (k == 0 && n == 0) result.push_back(now);
+
+    if (n < i || k == 0 || i > 9) return;
+
+    bt(k, n, i + 1, now, result);
+
+    now.push_back(i);
+    bt(k - 1, n - i, i + 1, now, result);
+    now.pop_back();
+}
