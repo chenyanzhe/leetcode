af82d25438bcffb1c68146fc746086daff7815be
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-12 00:56:44 +0800

    Problem 222. Count Complete Tree Nodes

---
+++ include/CountCompleteTreeNodes.hpp
@@ -0,0 +1,11 @@
+#ifndef COUNT_COMPLETE_TREE_NODES_HPP_
+#define COUNT_COMPLETE_TREE_NODES_HPP_
+
+#include "TreeNode.hpp"
+
+class CountCompleteTreeNodes {
+public:
+    int countNodes(TreeNode* root);
+};
+
+#endif // COUNT_COMPLETE_TREE_NODES_HPP_
---
+++ src/CountCompleteTreeNodes.cpp
@@ -0,0 +1,17 @@
+#include "CountCompleteTreeNodes.hpp"
+
+int CountCompleteTreeNodes::countNodes(TreeNode* root)
+{
+    if (root == nullptr) return 0;
+    if (root->left == nullptr) return 1;
+    if (root->right == nullptr) return 2;
+
+    TreeNode* ln = root; int ld = 0;
+    while (ln != nullptr) { ld++; ln = ln->left; }
+
+    TreeNode* rn = root; int rd = 0;
+    while (rn != nullptr) { rd++; rn = rn->right; }
+
+    if (ld == rd) return (1 << ld) - 1;
+    else return 1 + countNodes(root->left) + countNodes(root->right);
+}
