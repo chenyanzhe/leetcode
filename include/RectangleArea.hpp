1a3007a4d9410cbb7d7b9525d5757d76334e9cac
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-13 20:05:09 +0800

    Problem 223. Rectangle Area

---
+++ include/RectangleArea.hpp
@@ -0,0 +1,18 @@
+#ifndef RECTANGLE_AREA_HPP_
+#define RECTANGLE_AREA_HPP_
+
+class RectangleArea {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H);
+
+private:
+    enum RELATION { OVERLAP, DISJOINT, CONTAIN };
+
+    RELATION decide(int a1, int a2, int b1, int b2);
+
+    int area(int a1, int a2, int b1, int b2);
+
+    int share(int a1, int a2, int b1, int b2);
+};
+
+#endif // RECTANGLE_AREA_HPP_
---
+++ tests/RectangleAreaTest.cpp
@@ -0,0 +1,11 @@
+#include "catch.hpp"
+
+#include "RectangleArea.hpp"
+
+TEST_CASE("Rectangle Area") {
+    RectangleArea s;
+
+    SECTION("Sample test") {
+        REQUIRE(s.computeArea(-5, -2, 3, 1, -3, -3, 3, 3) == 42);
+    }
+}
