f11425b041889eee8558403fc7f300e950c08c35
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-18 20:54:02 +0800

    Problem 227. Basic Calculator II

---
+++ include/BasicCalculatorII.hpp
@@ -0,0 +1,23 @@
+#ifndef BASIC_CALCULATOR_II_HPP_
+#define BASIC_CALCULATOR_II_HPP_
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class BasicCalculatorII {
+public:
+    int calculate(string s);
+private:
+    typedef struct Token {
+        enum { OPERATOR, OPERAND } type;
+        union {
+            char op;
+            int val;
+        } tk;
+    } Token;
+
+    vector<Token> parse(string s);
+};
+
+#endif // BASIC_CALCULATOR_II_HPP_
