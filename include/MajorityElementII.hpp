03a3ad5e369c1acad9311c2d8d8708d750531175
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-21 10:38:17 +0800

    Problem 229. Majority Element II

---
+++ include/MajorityElementII.hpp
@@ -0,0 +1,12 @@
+#ifndef MAJORITY_ELEMENT_II_HPP_
+#define MAJORITY_ELEMENT_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class MajorityElementII {
+public:
+    vector<int> majorityElement(vector<int>& nums);
+};
+
+#endif // MAJORITY_ELEMENT_II_HPP_
