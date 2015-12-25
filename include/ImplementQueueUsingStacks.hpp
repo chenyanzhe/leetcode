a8cee11df17c58dfacd485cd471f2d4b87a64110
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-25 15:14:29 +0800

    Problem 232. Implement Queue using Stacks

---
+++ include/ImplementQueueUsingStacks.hpp
@@ -0,0 +1,26 @@
+#ifndef IMPLEMENT_QUEUE_USING_STACKS_HPP_
+#define IMPLEMENT_QUEUE_USING_STACKS_HPP_
+
+#include <stack>
+using namespace std;
+
+class ImplementQueueUsingStacks {
+public:
+  // Push element x to the back of queue.
+  void push(int x);
+  
+  // Removes the element from in front of queue.
+  void pop(void);
+  
+  // Get the front element.
+  int peek(void);
+  
+  // Return whether the queue is empty.
+  bool empty(void);
+
+private:
+  stack<int> primary;
+  stack<int> backup;
+};
+
+#endif // IMPLEMENT_QUEUE_USING_STACKS_HPP_
---
+++ tests/ImplementQueueUsingStacksTest.cpp
@@ -0,0 +1,18 @@
+#include "catch.hpp"
+
+#include "ImplementQueueUsingStacks.hpp"
+
+TEST_CASE("Implement Queue Using Stacks") {
+    ImplementQueueUsingStacks s;
+
+    SECTION("Sample test") {
+        REQUIRE(s.empty());
+        s.push(1);
+        REQUIRE(s.peek() == 1);
+        s.push(2);
+        REQUIRE(s.peek() == 1);
+        s.pop();
+        REQUIRE(s.peek() == 2);
+        REQUIRE_FALSE(s.empty());
+    }
+}
