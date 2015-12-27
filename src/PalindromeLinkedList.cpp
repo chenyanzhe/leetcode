1cb4857cadb760e74f97cc56454d341686c9112d
Yanzhe Chen <yanzhe.cn@gmail.com>
2015-12-27 16:46:33 +0800

    Problem 234. Palindrome Linked List

---
+++ src/PalindromeLinkedList.cpp
@@ -0,0 +1,51 @@
+#include "PalindromeLinkedList.hpp"
+
+bool PalindromeLinkedList::isPalindrome(ListNode* head)
+{
+  ListNode* p = head;
+  int len = 0;
+  
+  while (p != nullptr) {
+    len++;
+    p = p->next;
+  }
+  
+  if (len == 0 || len == 1) return true;
+  if (len == 2) return head->val == head->next->val;
+  
+  ListNode *prev = nullptr, *l, *r;
+  if (len % 2 == 1) {
+    p = head;
+    for (int i = 0; i <= len / 2; i++) {
+      ListNode* next = p->next;
+      p->next = prev;
+      prev = p;
+      p = next;
+    }
+    l = prev->next;
+    r = p;
+    while (l != nullptr) {
+      if (l->val != r->val) return false;
+      l = l->next;
+      r = r->next;
+    }
+    return true;
+  }
+  else {
+    p = head;
+    for (int i = 0; i < len / 2; i++) {
+      ListNode* next = p->next;
+      p->next = prev;
+      prev = p;
+      p = next;
+    }
+    ListNode* l = prev;
+    ListNode* r = p;
+    while (l != nullptr) {
+      if (l->val != r->val) return false;
+      l = l->next;
+      r = r->next;
+    }
+    return true;
+  }
+}
---
+++ tests/PalindromeLinkedListTest.cpp
@@ -0,0 +1,20 @@
+#include "catch.hpp"
+
+#include "PalindromeLinkedList.hpp"
+
+TEST_CASE("Palindrome Linked List") {
+    PalindromeLinkedList s;
+
+    ListNode *head = nullptr;
+    
+    SECTION("Sample test") {
+      ListNode *_1 = new ListNode(1); _1->next = head; head = _1;
+      ListNode *_2 = new ListNode(2); _2->next = head; head = _2;
+      ListNode *_2_1 = new ListNode(2); _2_1->next = head; head = _2_1;
+      ListNode *_1_1 = new ListNode(1); _1_1->next = head; head = _1_1;
+      
+      REQUIRE(s.isPalindrome(head));
+    }
+    
+    list_free(head);
+}
