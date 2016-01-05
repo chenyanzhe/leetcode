0800b53b4e5daf0ab941c6ed8a2a93ec366b98fa
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-01-05 21:40:33 +0800

    Problem 241. Different Ways to Add Parentheses

---
+++ include/DifferentWaysToAddParentheses.hpp
@@ -0,0 +1,13 @@
+#ifndef DIFFERENT_WAYS_TO_ADD_PARENTHESES_HPP_
+#define DIFFERENT_WAYS_TO_ADD_PARENTHESES_HPP_
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class DifferentWaysToAddParentheses {
+public:
+  vector<int> diffWaysToCompute(string input);  
+};
+
+#endif // DIFFERENT_WAYS_TO_ADD_PARENTHESES_HPP_
---
+++ tests/DifferentWaysToAddParenthesesTest.cpp
@@ -0,0 +1,20 @@
+#include "catch.hpp"
+
+#include "DifferentWaysToAddParentheses.hpp"
+
+#include <algorithm>
+using namespace std;
+
+TEST_CASE("Different Ways to Add Parentheses") {
+    DifferentWaysToAddParentheses s;
+
+    SECTION("Sample test") {
+        string input = "2*3-4*5";
+        vector<int> expected {-34, -14, -10, -10, 10};
+        vector<int> result = s.diffWaysToCompute(input);
+        
+        REQUIRE(result.size() == expected.size());
+        for (int i = 0; i < result.size(); i++)
+          REQUIRE_FALSE(find(expected.begin(), expected.end(), result[i]) == expected.end());
+    }
+}
