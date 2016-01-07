f72a9aebc85124181effdce6c1194efbc0406cf0
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-01-07 20:01:21 +0800

    Problem 260. Single Number III

---
+++ include/SingleNumberIII.hpp
@@ -0,0 +1,12 @@
+#ifndef SINGLE_NUMBER_III_HPP_
+#define SINGLE_NUMBER_III_HPP_
+
+#include <vector>
+using namespace std;
+
+class SingleNumberIII {
+public:
+  vector<int> singleNumber(vector<int>& nums);  
+};
+
+#endif // SINGLE_NUMBER_III_HPP_
---
+++ src/SingleNumberIII.cpp
@@ -0,0 +1,20 @@
+#include "SingleNumberIII.hpp"
+
+vector<int> SingleNumberIII::singleNumber(vector<int>& nums)
+{
+  int flag = 0;
+  for (auto num : nums)
+    flag ^= num;
+  
+  flag &= -flag;
+  
+  vector<int> ret {0, 0};
+  for (auto num : nums) {
+    if (num & flag)
+      ret[0] ^= num;
+    else
+      ret[1] ^= num;
+  }
+  
+  return ret;
+}
