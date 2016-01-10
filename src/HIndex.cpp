495db0f8a198a34e88c50f0448cec79df3e9cdf2
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-01-10 13:55:05 +0800

    Problem 274. H-Index
    Refine solution.

--- src/HIndex.cpp
+++ src/HIndex.cpp
@@ -5,10 +5,22 @@ using namespace std;
 
 int HIndex::hIndex(vector<int>& citations)
 {
-  sort(citations.begin(), citations.end());
   int n = citations.size();
-  int i = n - 1;
 
-  while (i >= 0 && citations[i] >= n - i) i--;
-  return n - (i + 1);
+  vector<int> count(n + 1, 0);
+
+  for (auto cite : citations) {
+    if (cite <= n) count[cite]++;
+    else count[n]++;
+  }
+
+  int h   = n;
+  int acc = 0;
+
+  while (acc + count[h] < h) {
+    acc += count[h];
+    h--;
+  }
+
+  return h;
 }
