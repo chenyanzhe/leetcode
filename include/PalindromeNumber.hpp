3f359376e274c0ed77d06fe4fa32238f8e076ac2
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-01-27 16:47:05 +0800

    Refine Problem 9. Palindrome Number.

--- include/PalindromeNumber.hpp
+++ include/PalindromeNumber.hpp
@@ -5,13 +5,6 @@ class PalindromeNumber
 {
 public:
   bool isPalindrome(int x);
-private:
-  // Get integer x's ith digit, start from the lowest digit
-  //   e.g. given x = 321, getDigit(x, 1) = 1, getDigit(x, 3) = 3
-  // Suppose both x and i are positive
-  // When i exceeds MAX_INT's digits number, return 0
-  //   e.g. getDigit(x, 11) = 0 when int is 32-bit
-  int getDigit(int x, int i);
 };
 
 #endif // PALINDROME_NUMBER_HPP_
--- src/PalindromeNumber.cpp
+++ src/PalindromeNumber.cpp
@@ -6,38 +6,14 @@ bool PalindromeNumber::isPalindrome(int x)
     return false;
   }
 
-  if (0 <= x && x <= 9) {
-    return true;
+  int p = x;
+  int q = 0;
+
+  while (p >= 10) {
+    q *= 10;
+    q += p % 10;
+    p /= 10;
   }
 
-  int digits = 0;
-  int _x = x;
-
-  while (_x > 0) {
-    digits++;
-    _x /= 10;
-  }
-
-  for (int i = 1, j = digits; i < j; i++, j--) {
-    if (getDigit(x, i) != getDigit(x, j)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-int PalindromeNumber::getDigit(int x, int i)
-{
-  if (i > 10) {
-    return 0;
-  }
-
-  int div = 1;
-
-  for (int j = 0; j < i - 1; j++) {
-    div *= 10;
-  }
-
-  return x / div % 10;
+  return q == x / 10 && p == x % 10;
 }
