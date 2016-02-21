2f756ebd5a4d5bd4cfbc8764870b3641b32bcbb0
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 17:36:02 +0800

    Refine Problem 57. Insert Interval.

--- src/InsertInterval.cpp
+++ src/InsertInterval.cpp
@@ -1,42 +1,26 @@
 #include "InsertInterval.hpp"
 
-#include <algorithm>
-using namespace std;
-
 vector<Interval> InsertInterval::insert(vector<Interval>& intervals,
                                         Interval newInterval)
 {
-  intervals.push_back(newInterval);
-  sort(intervals.begin(), intervals.end(),
-  [](const Interval & a, const Interval & b) {
-    return a.start < b.start;
-  });
-  bool changed = false;
-  vector<Interval> origin = intervals;
-  vector<Interval> backup;
+  vector<Interval> res;
+  int index = 0;
 
-  do {
-    changed = false;
+  while(index < intervals.size() && intervals[index].end < newInterval.start) {
+    res.push_back(intervals[index++]);
+  }
 
-    if (origin.size() <= 1) {
-      break;
-    }
+  while(index < intervals.size() && intervals[index].start <= newInterval.end) {
+    newInterval.start = min(newInterval.start, intervals[index].start);
+    newInterval.end = max(newInterval.end, intervals[index].end);
+    index++;
+  }
 
-    for (int i = 0; i < origin.size() - 1; i++) {
-      if (origin[i].end < origin[i + 1].start) {
-        backup.push_back(origin[i]);
-      } else {
-        origin[i + 1].start = origin[i].start;
-        origin[i + 1].end = (origin[i + 1].end > origin[i].end) ? origin[i + 1].end :
-                            origin[i].end;
-        changed = true;
-      }
-    }
+  res.push_back(newInterval);
 
-    backup.push_back(origin[origin.size() - 1]);
-    origin = backup;
-    backup.clear();
-  } while (changed);
+  while(index < intervals.size()) {
+    res.push_back(intervals[index++]);
+  }
 
-  return origin;
+  return res;
 }
