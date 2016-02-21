e150af317db1dafa128e998363a7916cb2d2479d
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 15:27:32 +0800

    Refine Problem 53. Maximum Subarray.

--- include/MaximumSubarray.hpp
+++ include/MaximumSubarray.hpp
@@ -8,6 +8,9 @@ class MaximumSubarray
 {
 public:
   int maxSubArray(vector<int>& nums);
+private:
+  int maxSubArray_DynamicProgramming(vector<int>& nums);
+  int maxSubArray_Array(vector<int>& nums);
 };
 
 #endif // MAXIMUM_SUBARRAY_HPP_
--- src/MaximumSubarray.cpp
+++ src/MaximumSubarray.cpp
@@ -2,6 +2,11 @@
 
 int MaximumSubarray::maxSubArray(vector<int>& nums)
 {
+  return maxSubArray_Array(nums);
+}
+
+int MaximumSubarray::maxSubArray_DynamicProgramming(vector<int>& nums)
+{
   vector<int> dp(nums.size(), 0);
   dp[0] = nums[0];
   int max = nums[0];
@@ -9,6 +14,19 @@ int MaximumSubarray::maxSubArray(vector<int>& nums)
   for (int i = 1; i < nums.size(); i++) {
     dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
     max = dp[i] > max ? dp[i] : max;
+  }
+
+  return max;
+}
+
+int MaximumSubarray::maxSubArray_Array(vector<int>& nums)
+{
+  int max = nums[0];
+  int maxEnding = nums[0];
+
+  for (int i = 1; i < nums.size(); i++) {
+    maxEnding = nums[i] + (maxEnding > 0 ? maxEnding : 0);
+    max = maxEnding > max ? maxEnding : max;
   }
 
   return max;
