833dd02eb2a0a3879a13efa3fafbfc42b9024207
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 21:21:53 +0800

    Refine Problem 62. Unique Paths.

--- include/UniquePaths.hpp
+++ include/UniquePaths.hpp
@@ -5,6 +5,9 @@ class UniquePaths
 {
 public:
   int uniquePaths(int m, int n);
+public:
+  int uniquePaths_Raw(int m, int n);
+  int uniquePaths_Optimized(int m, int n);
 };
 
 #endif // UNIQUE_PATHS_HPP_
--- src/UniquePaths.cpp
+++ src/UniquePaths.cpp
@@ -1,23 +1,17 @@
 #include "UniquePaths.hpp"
 
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int UniquePaths::uniquePaths(int m, int n)
 {
-  if (m <= 0 || n <= 0) {
-    return 0;
-  }
+  return uniquePaths_Optimized(m, n);
+}
 
-  vector<vector<int>> dp(m, vector<int>(n, 0));
-
-  for (int i = 0; i < m; i++) {
-    dp[i][0] = 1;
-  }
-
-  for (int j = 0; j < n; j++) {
-    dp[0][j] = 1;
-  }
+int UniquePaths::uniquePaths_Raw(int m, int n)
+{
+  vector<vector<int>> dp(m, vector<int>(n, 1));
 
   for (int i = 1; i < m; i++) {
     for (int j = 1; j < n; j++) {
@@ -26,4 +20,24 @@ int UniquePaths::uniquePaths(int m, int n)
   }
 
   return dp[m - 1][n - 1];
+}
+
+int UniquePaths::uniquePaths_Optimized(int m, int n)
+{
+  if (m > n) {
+    return uniquePaths_Optimized(n, m);
+  }
+
+  vector<int> pre(m, 1);
+  vector<int> cur(m, 1);
+
+  for (int j = 1; j < n; j++) {
+    for (int i = 1; i < m; i++) {
+      cur[i] = cur[i - 1] + pre[i];
+    }
+
+    swap(pre, cur);
+  }
+
+  return pre[m - 1];
 }
