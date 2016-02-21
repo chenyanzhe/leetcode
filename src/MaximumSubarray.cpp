562e61e47f8e3a9fa5fadf30ceea7954659ac7e5
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 15:14:15 +0800

    Refine Problem 53. Maximum Subarray.

--- src/MaximumSubarray.cpp
+++ src/MaximumSubarray.cpp
@@ -1,24 +1,15 @@
 #include "MaximumSubarray.hpp"
 
-#include <algorithm>
-using namespace std;
-
 int MaximumSubarray::maxSubArray(vector<int>& nums)
 {
-  int sz = nums.size();
-
-  if (sz == 0) {
-    return 0;
-  }
-
-  vector<int> dp(sz, 0);
+  vector<int> dp(nums.size(), 0);
   dp[0] = nums[0];
-  int ret = nums[0];
+  int max = nums[0];
 
-  for (int i = 1; i < sz; i++) {
-    dp[i] = max(dp[i - 1] + nums[i], nums[i]);
-    ret = max(dp[i], ret);
+  for (int i = 1; i < nums.size(); i++) {
+    dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+    max = dp[i] > max ? dp[i] : max;
   }
 
-  return ret;
+  return max;
 }
