9e902e41fc33b91f9092f8005f9f5f21655165ff
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 17:22:12 +0800

    Refine Problem 56. Merge Intervals.

--- src/MergeIntervals.cpp
+++ src/MergeIntervals.cpp
@@ -1,37 +1,33 @@
 #include "MergeIntervals.hpp"
 
+#include <algorithm>
+using namespace std;
+
 vector<Interval> MergeIntervals::merge(vector<Interval>& intervals)
 {
+  vector<Interval> res;
+
+  if (intervals.empty()) {
+    return res;
+  }
+
   sort(intervals.begin(), intervals.end(),
   [](const Interval & a, const Interval & b) -> bool {
     return a.start < b.start;
   });
-  bool changed = false;
-  vector<Interval> origin = intervals;
-  vector<Interval> backup;
+  int curstart = intervals[0].start;
+  int curend = intervals[0].end;
 
-  do {
-    changed = false;
-
-    if (origin.size() <= 1) {
-      break;
+  for (int i = 1; i < intervals.size(); i++) {
+    if (intervals[i].start > curend) {
+      res.emplace_back(curstart, curend);
+      curstart = intervals[i].start;
+      curend = intervals[i].end;
+    } else {
+      curend = max(intervals[i].end, curend);
     }
+  }
 
-    for (int i = 0; i < origin.size() - 1; i++) {
-      if (origin[i].end < origin[i + 1].start) {
-        backup.push_back(origin[i]);
-      } else {
-        origin[i + 1].start = origin[i].start;
-        origin[i + 1].end = (origin[i + 1].end > origin[i].end) ? origin[i + 1].end :
-                            origin[i].end;
-        changed = true;
-      }
-    }
-
-    backup.push_back(origin[origin.size() - 1]);
-    origin = backup;
-    backup.clear();
-  } while (changed);
-
-  return origin;
+  res.emplace_back(curstart, curend);
+  return res;
 }
