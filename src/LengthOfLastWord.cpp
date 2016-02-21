a007dc3aa1eff3ca22b9ef68603a0e595837885d
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 19:49:00 +0800

    Refine Problem 58. Length of Last Word.

--- src/LengthOfLastWord.cpp
+++ src/LengthOfLastWord.cpp
@@ -2,15 +2,13 @@
 
 int LengthOfLastWord::lengthOfLastWord(string s)
 {
-  bool inWord = false;
   int len = 0;
 
   for (int i = 0; i < s.size(); i++) {
-    if (s[i] == ' ' && inWord) {
-      inWord = false;
-    } else if (s[i] != ' ') {
-      len = inWord ? len + 1 : 1;
-      inWord = true;
+    if (s[i] != ' ') {
+      len++;
+    } else if (i + 1 < s.size() && s[i + 1] != ' ') {
+      len = 0;
     }
   }
 
