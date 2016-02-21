650a29c153377215d2855d7617c2f6c9e30a7439
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-21 16:44:16 +0800

    Refine Problem 55. Jump Game.

--- src/JumpGame.cpp
+++ src/JumpGame.cpp
@@ -2,23 +2,15 @@
 
 bool JumpGame::canJump(vector<int>& nums)
 {
-  if (nums.size() == 0) {
-    return false;
-  }
-
-  if (nums.size() == 1) {
-    return true;
-  }
-
-  int canReach = 0;
+  int right = 0;
 
   for (int i = 0; i < nums.size(); i++) {
-    if (i > canReach) {
-      break;
+    if (i > right) {
+      return false;
     }
 
-    canReach = (nums[i] + i > canReach) ? nums[i] + i : canReach;
+    right = max(i + nums[i], right);
   }
 
-  return canReach >= nums.size() - 1;
+  return true;
 }
