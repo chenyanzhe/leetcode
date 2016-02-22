2732bd3e593ccdef6d0bc0d7cd673aa215145c13
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-22 21:34:22 +0800

    Refine Problem 71. Simplify Path.

--- include/SimplifyPath.hpp
+++ include/SimplifyPath.hpp
@@ -2,15 +2,12 @@
 #define SIMPLIFY_PATH_HPP_
 
 #include <string>
-#include <vector>
 using namespace std;
 
 class SimplifyPath
 {
 public:
   string simplifyPath(string path);
-private:
-  void tokenize(const string& path, vector<string>& tokens);
 };
 
 #endif // SIMPLIFY_PATH_HPP_
--- src/SimplifyPath.cpp
+++ src/SimplifyPath.cpp
@@ -1,62 +1,28 @@
 #include "SimplifyPath.hpp"
 
-#include <stack>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 string SimplifyPath::simplifyPath(string path)
 {
-  vector<string> tokens;
-  tokenize(path, tokens);
-  string result = "";
-  stack<string> ops;
-
-  for (auto t : tokens) {
-    if (t == "..") {
-      if (!ops.empty()) {
-        ops.pop();
-      }
-    } else if (t != ".") {
-      ops.push(t);
+  string res, t;
+  vector<string> stk;
+  stringstream ss(path);
+  
+  while (getline(ss, t, '/')) {
+    if (t == "" || t == ".") {
+      continue;
+    } else if (t == ".." && !stk.empty()) {
+      stk.pop_back();
+    } else if (t != "..") {
+      stk.push_back(t);
     }
   }
-
-  if (ops.empty()) {
-    return "/";
+  
+  for (auto t : stk) {
+    res += "/" + t;
   }
 
-  while (!ops.empty()) {
-    string t = ops.top();
-    ops.pop();
-    result = "/" + t + result;
-  }
-
-  return result;
-}
-
-void SimplifyPath::tokenize(const string& path, vector<string>& tokens)
-{
-  int i = 0, j = 0;
-  int sz = path.size();
-
-  if (sz == 0) {
-    return;
-  }
-
-  while (i < sz) {
-    while (i < sz && path[i] == '/') {
-      i++;
-    }
-
-    j = i;
-
-    while (j < sz && path[j] != '/') {
-      j++;
-    }
-
-    if (i < sz) {
-      tokens.push_back(path.substr(i, j - i));
-    }
-
-    i = j + 1;
-  }
+  return res.empty() ? "/" : res;
 }
