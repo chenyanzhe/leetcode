a55c525e07d92e64b996476bec2f17915450736b
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-22 20:33:44 +0800

    Refine Problem 66. Plus One.

--- src/PlusOne.cpp
+++ src/PlusOne.cpp
@@ -2,37 +2,18 @@
 
 vector<int> PlusOne::plusOne(vector<int>& digits)
 {
-  int sz = digits.size();
+  int n = digits.size();
 
-  if (sz == 0) {
-    return digits;
-  }
-
-  bool carry = true;
-
-  for (int i = sz - 1; i >= 0; i--) {
-    if (carry) {
-      digits[i]++;
-    }
-
-    carry = (digits[i] >= 10);
-
-    if (carry) {
-      digits[i] -= 10;
+  for (int i = n - 1; i >= 0; --i) {
+    if (digits[i] == 9) {
+      digits[i] = 0;
     } else {
-      break;
+      digits[i]++;
+      return digits;
     }
   }
 
-  if (!carry) {
-    return digits;
-  }
-
-  vector<int> ret {1};
-
-  for (int i = 0; i < sz; i++) {
-    ret.push_back(digits[i]);
-  }
-
-  return ret;
+  digits[0] = 1;
+  digits.push_back(0);
+  return digits;
 }
