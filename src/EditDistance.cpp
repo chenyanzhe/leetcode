ddd6a97506f577f7f6c43dabbff43ca03dc4a61e
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-23 09:12:06 +0800

    Refine Problem 72. Edit Distance.

--- src/EditDistance.cpp
+++ src/EditDistance.cpp
@@ -6,28 +6,58 @@ using namespace std;
 
 int EditDistance::minDistance(string word1, string word2)
 {
-  int sz1 = word1.size();
-  int sz2 = word2.size();
-  vector<vector<int>> dp(sz1 + 1, vector<int>(sz2 + 1, 0));
+  return minDistance_Optimized(word1, word2);
+}
 
-  for (int i = 0; i <= sz1; i++) {
+int EditDistance::minDistance_Raw(string word1, string word2)
+{
+  int m = word1.length(), n = word2.length();
+  vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+  for (int i = 1; i <= m; i++) {
     dp[i][0] = i;
   }
 
-  for (int j = 0; j <= sz2; j++) {
+  for (int j = 1; j <= n; j++) {
     dp[0][j] = j;
   }
 
-  for (int i = 1; i <= sz1; i++) {
-    for (int j = 1; j <= sz2; j++) {
+  for (int i = 1; i <= m; i++) {
+    for (int j = 1; j <= n; j++) {
       if (word1[i - 1] == word2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1];  // exactly match
-      } else
-        dp[i][j] = min(dp[i - 1][j - 1] + 1, // replace word1[i - 1] by word2[j - 1]
-                       min(dp[i][j - 1] + 1, // insert word2[j - 1] at word1[i - 1]
-                           dp[i - 1][j] + 1)); // delete word1[i - 1]
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = min(dp[i - 1][j - 1] + 1, min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+      }
     }
   }
 
-  return dp[sz1][sz2];
+  return dp[m][n];
+}
+
+int EditDistance::minDistance_Optimized(string word1, string word2)
+{
+  int m = word1.length(), n = word2.length();
+  vector<int> pre(m + 1, 0);
+  vector<int> cur(m + 1, 0);
+
+  for (int i = 1; i <= m; i++) {
+    pre[i] = i;
+  }
+
+  for (int j = 1; j <= n; j++) {
+    cur[0] = j;
+
+    for (int i = 1; i <= m; i++) {
+      if (word1[i - 1] == word2[j - 1]) {
+        cur[i] = pre[i - 1];
+      } else {
+        cur[i] = min(pre[i - 1] + 1, min(pre[i] + 1, cur[i - 1] + 1));
+      }
+    }
+
+    swap(pre, cur);
+  }
+
+  return pre[m];
 }
