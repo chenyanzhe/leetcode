0cc62250e1cae393c1791c5edd9ac9c36b6af24d
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-02-23 21:26:45 +0800

    Refine Problem 73. Set Matrix Zeroes.

--- src/SetMatrixZeroes.cpp
+++ src/SetMatrixZeroes.cpp
@@ -1,42 +1,28 @@
 #include "SetMatrixZeroes.hpp"
 
-#include <set>
-using namespace std;
-
 void SetMatrixZeroes::setZeroes(vector<vector<int>>& matrix)
 {
-  set<int> row;
-  set<int> col;
-  int nrow = matrix.size();
+  int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
 
-  if (nrow == 0) {
-    return;
-  }
+  for (int i = 0; i < rows; i++) {
+    if (matrix[i][0] == 0) {
+      col0 = 0;
+    }
 
-  int ncol = matrix[0].size();
-
-  if (ncol == 0) {
-    return;
-  }
-
-  for (int i = 0; i < nrow; i++) {
-    for (int j = 0; j < ncol; j++) {
+    for (int j = 1; j < cols; j++)
       if (matrix[i][j] == 0) {
-        row.insert(i);
-        col.insert(j);
+        matrix[i][0] = matrix[0][j] = 0;
       }
-    }
   }
 
-  for (auto r : row) {
-    for (int j = 0; j < ncol; j++) {
-      matrix[r][j] = 0;
-    }
-  }
+  for (int i = rows - 1; i >= 0; i--) {
+    for (int j = cols - 1; j >= 1; j--)
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
 
-  for (auto c : col) {
-    for (int i = 0; i < nrow; i++) {
-      matrix[i][c] = 0;
+    if (col0 == 0) {
+      matrix[i][0] = 0;
     }
   }
 }
