4b5b44e94ca235cabb7cb8ef83ed259696d12492
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-05-31 23:44:20 +0800

    Problem 324. Wiggle Sort II

---
+++ include/WiggleSortII.hpp
@@ -0,0 +1,15 @@
+#ifndef WIGGLE_SORT_II_HPP_
+#define WIGGLE_SORT_II_HPP_
+
+#include <vector>
+using namespace std;
+
+class WiggleSortII
+{
+public:
+  void wiggleSort(vector<int>& nums);
+private:
+  int m(int idx, int n);
+};
+
+#endif // WIGGLE_SORT_II_HPP_
