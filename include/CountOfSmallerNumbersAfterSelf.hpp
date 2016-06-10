2ade8817a06d8d415702b1c5fff79b0714846c06
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-10 22:09:24 +0800

    Problem 315. Count of Smaller Numbers After Self

---
+++ include/CountOfSmallerNumbersAfterSelf.hpp
@@ -0,0 +1,17 @@
+#ifndef COUNT_OF_SMALLER_NUMBERS_AFTER_SELF_HPP_
+#define COUNT_OF_SMALLER_NUMBERS_AFTER_SELF_HPP_
+
+#include <vector>
+using namespace std;
+
+class CountOfSmallerNumbersAfterSelf
+{
+public:
+  vector<int> countSmaller(vector<int>& nums);
+private:
+  int query(long index);
+  void update(long index, int val);
+  vector<int> bit;
+};
+
+#endif // COUNT_OF_SMALLER_NUMBERS_AFTER_SELF_HPP_
