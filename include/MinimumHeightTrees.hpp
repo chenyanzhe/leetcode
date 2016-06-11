66ca79a58cbdf7f291e059bfabc7d8b71ac9e000
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-11 23:36:54 +0800

    Problem 310. Minimum Height Trees

---
+++ include/MinimumHeightTrees.hpp
@@ -0,0 +1,14 @@
+#ifndef MINIMUM_HEIGHT_TREES_HPP_
+#define MINIMUM_HEIGHT_TREES_HPP_
+
+#include <vector>
+#include <utility>
+using namespace std;
+
+class MinimumHeightTrees
+{
+public:
+  vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges);
+};
+
+#endif // MINIMUM_HEIGHT_TREES_HPP_
---
+++ src/MinimumHeightTrees.cpp
@@ -0,0 +1,45 @@
+#include "MinimumHeightTrees.hpp"
+
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+vector<int> MinimumHeightTrees::findMinHeightTrees(int n,
+    vector<pair<int, int>>& edges)
+{
+  // no edges
+  if (n == 1) return vector<int> {0};
+
+  unordered_map<int, unordered_set<int>> adj;
+
+  for (auto e : edges) {
+    adj[e.first].insert(e.second);
+    adj[e.second].insert(e.first);
+  }
+
+  vector<int> leaves;
+
+  for (auto e : adj)
+    if (e.second.size() == 1) {
+      leaves.push_back(e.first);
+    }
+
+  while (n > 2) {
+    n -= leaves.size(); // eat up all the leaves
+    vector<int> new_leaves;
+
+    for (auto leaf : leaves) {
+      int neighbor = *(adj[leaf].begin());
+      // erase leaf node from neighbor
+      adj[neighbor].erase(leaf);
+
+      if (adj[neighbor].size() == 1) {
+        new_leaves.push_back(neighbor);
+      }
+    }
+
+    leaves = new_leaves;
+  }
+
+  return leaves;
+}
