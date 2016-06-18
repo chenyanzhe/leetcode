e108d520a74a8f83e4a8b793e237f196487fe7ff
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-18 21:09:27 +0800

    Refine Problem 64. Minimum Path Sum

--- src/MinimumPathSum.cpp
+++ src/MinimumPathSum.cpp
@@ -18,23 +18,21 @@ int MinimumPathSum::minPathSum(vector<vector<int>>& grid)
     return 0;
   }
 
-  vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
-  dp[0][0] = grid[0][0];
-
-  for (int i = 1; i < m; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0];
-  }
+  vector<int> dp(n, INT_MAX);
+  dp[0] = grid[0][0];
 
   for (int j = 1; j < n; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j];
+    dp[j] = dp[j - 1] + grid[0][j];
   }
 
   for (int i = 1; i < m; i++) {
+    dp[0] += grid[i][0];
+
     for (int j = 1; j < n; j++) {
-      dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+      dp[j] = min(dp[j - 1], dp[j]) + grid[i][j];
     }
   }
 
-  return dp[m - 1][n - 1];
+  return dp[n - 1];
 }
 
