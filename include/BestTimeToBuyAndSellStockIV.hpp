5148226f4d1fc74744ff130217b0834d7be6d446
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-19 16:40:33 +0800

    Refine Problem 188. Best Time to Buy and Sell Stock IV

--- include/BestTimeToBuyAndSellStockIV.hpp
+++ include/BestTimeToBuyAndSellStockIV.hpp
@@ -8,8 +8,6 @@ class BestTimeToBuyAndSellStockIV
 {
 public:
   int maxProfit(int k, vector<int>& prices);
-private:
-  int maxProfitNoLimits(vector<int>& prices);
 };
 
 #endif // BEST_TIME_TO_BUY_AND_SELL_STOCK_IV_HPP_
--- src/BestTimeToBuyAndSellStockIV.cpp
+++ src/BestTimeToBuyAndSellStockIV.cpp
@@ -5,49 +5,36 @@ using namespace std;
 
 int BestTimeToBuyAndSellStockIV::maxProfit(int k, vector<int>& prices)
 {
-  /**
-   * local[i][j]: i day, j txn, jth txn happened at day i
-   * global[i][j]: i day, j txn
-   *
-   * diff = prices[i] - prices[i-1]
-   * local[i][j] = max(global[i-1][j-1] + max(diff, 0), local[i-1][j] + diff)
-   * global[i][j] = max(global[i-1][j], local[i][j])
-   */
-  int days = prices.size();
+  int n = prices.size();
 
-  if (days <= 1) {
+  if (n <= 1) {
     return 0;
   }
 
-  if (k >= days) {
-    return maxProfitNoLimits(prices);
+  if (k >= n / 2) {
+    // quick solve path
+    int ret = 0;
+
+    for (int i = 1; i < n; i++) {
+      // as long as there is a price gap, we gain a profit
+      if (prices[i] > prices[i - 1]) {
+        ret += prices[i] - prices[i - 1];
+      }
+    }
+
+    return ret;
   }
 
-  vector<int> local(k + 1, 0);
-  vector<int> global(k + 1, 0);
+  vector<vector<int>> dp(k + 1, vector<int>(n, 0));
 
-  for (int i = 1; i < days; i++) {
-    for (int j = k; j >= 1; j--) {
-      int diff = prices[i] - prices[i - 1];
-      local[j] = max(global[j - 1] + max(diff, 0), local[j] + diff);
-      global[j] = max(global[j], local[j]);
+  for (int i = 1; i <= k; i++) {
+    int maxV = dp[i - 1][0] - prices[0];
+
+    for (int j = 1; j < n; j++) {
+      dp[i][j] = max(dp[i][j - 1], prices[j] + maxV);
+      maxV = max(maxV, dp[i - 1][j] - prices[j]);
     }
   }
 
-  return global[k];
-}
-
-int BestTimeToBuyAndSellStockIV::maxProfitNoLimits(vector<int>& prices)
-{
-  int result = 0;
-
-  for (int i = 1; i < prices.size(); i++) {
-    int diff = prices[i] - prices[i - 1];
-
-    if (diff > 0) {
-      result += diff;
-    }
-  }
-
-  return result;
+  return dp[k][n - 1];
 }
