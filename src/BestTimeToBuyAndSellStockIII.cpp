0f2176ad64c82dc6cefe1e5010295266466513b8
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-19 12:36:45 +0800

    Refine Problem 123. Best Time to Buy and Sell Stock III

--- src/BestTimeToBuyAndSellStockIII.cpp
+++ src/BestTimeToBuyAndSellStockIII.cpp
@@ -5,30 +5,23 @@ using namespace std;
 
 int BestTimeToBuyAndSellStockIII::maxProfit(vector<int>& prices)
 {
-  int sz = prices.size();
+  int n = prices.size();
 
-  if (sz <= 1) {
+  if (n <= 1) {
     return 0;
   }
 
-  vector<int> f(sz, 0);
-  vector<int> g(sz, 0);
+  int k = 2;
+  vector<vector<int>> dp(k + 1, vector<int>(n, 0));
 
-  for (int i = 1, min_price = prices[0]; i < sz; i++) {
-    f[i] = max(f[i - 1], prices[i] - min_price);
-    min_price = min(min_price, prices[i]);
+  for (int i = 1; i <= k; i++) {
+    int maxV = dp[i - 1][0] - prices[0];
+
+    for (int j = 1; j < n; j++) {
+      dp[i][j] = max(dp[i][j - 1], prices[j] + maxV);
+      maxV = max(maxV, dp[i - 1][j] - prices[j]);
+    }
   }
 
-  for (int i = sz - 2, max_price = prices[sz - 1]; i >= 0; i--) {
-    g[i] = max(g[i + 1], max_price -  prices[i]);
-    max_price = max(max_price, prices[i]);
-  }
-
-  int result = 0;
-
-  for (int i = 0; i < sz; i++) {
-    result = max(result, f[i] + g[i]);
-  }
-
-  return result;
+  return dp[k][n - 1];
 }
