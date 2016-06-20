2204035d3cf5281355664f314cd967a088886ec1
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-20 19:55:45 +0800

    Refine Problem 300. Longest Increasing Subsequence

--- src/LongestIncreasingSubsequence.cpp
+++ src/LongestIncreasingSubsequence.cpp
@@ -1,27 +1,35 @@
 #include "LongestIncreasingSubsequence.hpp"
 
+#include <algorithm>
+#include <set>
+using namespace std;
+
 int LongestIncreasingSubsequence::lengthOfLIS(vector<int>& nums)
 {
-  int sz = nums.size();
+  int n = nums.size();
+  set<int> s(nums.begin(), nums.end());
+  vector<int> snums;
 
-  if (sz == 0) {
-    return 0;
-  }
+  snums.assign(s.begin(), s.end());
+  int m = snums.size();
 
-  int ret = 1;
-  vector<int> dp(sz, 1);
+  vector<int> dp(m + 1, 0);
 
-  for (int i = 1; i < sz; i++) {
-    for (int j = 0; j < i; j++) {
-      if (nums[i] > nums[j] && dp[j] + 1 > dp[i]) {
-        dp[i] = dp[j] + 1;
+  for (int i = 1; i <= n; i++) {
+    int newv = dp[0];
 
-        if (ret < dp[i]) {
-          ret = dp[i];
-        }
+    for (int j = 1; j <= m; j++) {
+      int oldv = dp[j];
+
+      if (nums[i - 1] == snums[j - 1]) {
+        dp[j] = newv + 1;
+      } else {
+        dp[j] = max(dp[j], dp[j - 1]);
       }
+
+      newv = oldv;
     }
   }
 
-  return ret;
+  return dp[m];
 }
--- tests/LongestIncreasingSubsequenceTest.cpp
+++ tests/LongestIncreasingSubsequenceTest.cpp
@@ -9,4 +9,8 @@ TEST_CASE("Longest Increasing Subsequence")
     vector<int> nums {10, 9, 2, 5, 3, 7, 101, 18};
     REQUIRE(s.lengthOfLIS(nums) == 4);
   }
+  SECTION("Non increasing sequence") {
+    vector<int> nums {2, 2};
+    REQUIRE(s.lengthOfLIS(nums) == 1);
+  }
 }
