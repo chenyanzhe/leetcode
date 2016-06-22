b9034754037ab4086dff4f1448419defedc64457
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-22 15:58:19 +0800

    Refine Problem 87. Scramble String

--- src/ScrambleString.cpp
+++ src/ScrambleString.cpp
@@ -1,7 +1,6 @@
 #include "ScrambleString.hpp"
 
-// #include <vector>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
 bool ScrambleString::isScramble(string s1, string s2)
@@ -10,47 +9,34 @@ bool ScrambleString::isScramble(string s1, string s2)
     return false;
   }
 
-  int sz = s1.size();
-
-  if (sz == 0) {
+  if (s1 == s2) {
     return true;
   }
 
-  // vector<vector<vector<bool>>> dp(sz, vector<vector<bool>>(sz, vector<bool>(sz, false)));
-  // for speed reason, do not use vectors
-  bool dp[sz][sz][sz];
-  fill_n(&dp[0][0][0], sz * sz * sz, false);
+  int n = s1.size();
+  vector<vector<vector<bool>>> dp(n, vector<vector<bool>>(n, vector<bool>(n,
+                                  false)));
 
-  for (int i = 0; i < sz; i++)
-    for (int j = 0; j < sz; j++) {
-      dp[0][i][j] = s1[i] == s2[j];
+  // dp[i][j][k]: s1[i:i+k] v.s. s2[j:j+k]
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) {
+      dp[i][j][0] = (s1[i] == s2[j]);
     }
 
-  for (int k = 1; k < sz; k++) {
-    for (int i = 0; i < sz - k; i++) {
-      for (int j = 0; j < sz - k; j++) {
-        // consider dp[k][i][j] => [i ... i+k] & [j ... j+k]
-        // split it to the following two parts:
-        // @ PART I @
-        // <[i], [j+k]> && <[i+1, i+k], [j, j+k-1]>
-        // <[i, i+1], [j+k-1, j+k]> && <[i+2, j+k], [j, j+k-2]>
-        // ...
-        // <[i, i+k-1], [j+1, j+k]> && <[i+k], [j]>
-        // @ PART II @
-        // <[i], [j]> && <[i+1, i+k], [j+1, j+k]>
-        // <[i, i+1], [j, j+1]> && <[i+2, j+k], [j+2, j+k]>
-        // ...
-        // <[i, i+k-1], [j, j+k-1]> && <[i+k], [j+k]>
-        for (int l = 0; l < k; l++) {
-          if ((dp[l][i][j + k - l] && dp[k - l - 1][i + l + 1][j]) ||
-              (dp[l][i][j] && dp[k - l - 1][i + l + 1][j + l + 1])) {
-            dp[k][i][j] = true;
-            break;
+  for (int k = 1; k < n; k++) {
+    for (int i = 0; i < n - k; i++) {
+      for (int j = 0; j < n - k; j++) {
+        for (int p = 0; p < k; p++) {
+          // s1[i:i+p] + s1[i+p+1:i+k] v.s. s2[j:j+p] + s2[j+p+1:j+k]
+          // s1[i:i+p] + s1[i+p+1:i+k] v.s. s2[j:j+k-p-1] + s2[j+k-p:j+k]
+          if ((dp[i][j][p] && dp[i + p + 1][j + p + 1][k - p - 1]) ||
+              (dp[i][j + k - p][p] && dp[i + p + 1][j][k - p - 1])) {
+            dp[i][j][k] = true;
           }
         }
       }
     }
   }
 
-  return dp[sz - 1][0][0];
+  return dp[0][0][n - 1];
 }
