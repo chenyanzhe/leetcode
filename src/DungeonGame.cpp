f7dbb18db0d7a38d5e18b7fb4dafd9ee7f7bec85
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-28 19:48:42 +0800

    Refine Problem 174. Dungeon Game

--- src/DungeonGame.cpp
+++ src/DungeonGame.cpp
@@ -16,32 +16,17 @@ int DungeonGame::calculateMinimumHP(vector<vector<int>>& dungeon)
     return 0;
 
   vector<vector<int>> dp(m, vector<int>(n, 0));
-  dp[m - 1][n - 1] = (dungeon[m - 1][n - 1] >= 0) ? 1 : (1 - dungeon[m - 1][n -
-                     1]);
+  dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1]);
+
+  for (int i = m - 2; i >= 0; i--)
+    dp[i][n - 1] = max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+
+  for (int j = n - 2; j >= 0; j--)
+    dp[m - 1][j] = max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
 
   for (int i = m - 2; i >= 0; i--) {
-    if (dp[i + 1][n - 1] - dungeon[i][n - 1] <= 0)
-      dp[i][n - 1] = 1;
-    else
-      dp[i][n - 1] = dp[i + 1][n - 1] - dungeon[i][n - 1];
-  }
-
-  for (int j = n - 2; j >= 0; j--) {
-    if (dp[m - 1][j + 1] - dungeon[m - 1][j] <= 0)
-      dp[m - 1][j] = 1;
-    else
-      dp[m - 1][j] = dp[m - 1][j + 1] - dungeon[m - 1][j];
-  }
-
-  for (int i = m - 2; i >= 0; i--) {
-    for (int j = n - 2; j >= 0; j--) {
-      int base = min(dp[i + 1][j], dp[i][j + 1]);
-
-      if (base - dungeon[i][j] <= 0)
-        dp[i][j] = 1;
-      else
-        dp[i][j] = base - dungeon[i][j];
-    }
+    for (int j = n - 2; j >= 0; j--)
+      dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
   }
 
   return dp[0][0];
