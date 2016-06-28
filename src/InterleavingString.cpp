3bf33489abfa94d064df728f2fd60e9de7446cbe
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-06-28 23:01:59 +0800

    Refine Problem 97. Interleaving String

--- src/InterleavingString.cpp
+++ src/InterleavingString.cpp
@@ -5,29 +5,25 @@ using namespace std;
 
 bool InterleavingString::isInterleave(string s1, string s2, string s3)
 {
-  int sz1 = s1.size();
-  int sz2 = s2.size();
-  int sz3 = s3.size();
+  int n1 = s1.size();
+  int n2 = s2.size();
+  int n3 = s3.size();
 
-  // fast path
-  if (sz1 + sz2 != sz3)
-    return false;
+  if (n1 + n2 != n3) return false;
 
-  vector<vector<bool>> dp(sz1 + 1, vector<bool>(sz2 + 1, false));
+  vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
   dp[0][0] = true;
 
-  for (int i = 1; i <= sz1; i++)
-    dp[i][0] = (s1[i - 1] == s3[i - 1]) ? dp[i - 1][0] : false;
+  for (int i = 1; i <= n1; i++)
+    dp[i][0] = (dp[i - 1][0] && (s1[i - 1] == s3[i - 1]));
 
-  for (int j = 1; j <= sz2; j++)
-    dp[0][j] = (s2[j - 1] == s3[j - 1]) ? dp[0][j - 1] : false;
+  for (int j = 1; j <= n2; j++)
+    dp[0][j] = (dp[0][j - 1] && (s2[j - 1] == s3[j - 1]));
 
-  for (int i = 1; i <= sz1; i++) {
-    for (int j = 1; j <= sz2; j++) {
+  for (int i = 1; i <= n1; i++)
+    for (int j = 1; j <= n2; j++)
       dp[i][j] = ((s1[i - 1] == s3[i + j - 1] && dp[i - 1][j]) ||
                   (s2[j - 1] == s3[i + j - 1] && dp[i][j - 1]));
-    }
-  }
 
-  return dp[sz1][sz2];
+  return dp[n1][n2];
 }
