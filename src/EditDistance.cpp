58171732183a8961ccaa1490f298164e9dd8eb94
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-05 21:07:37 +0800

    Refine Problem 72. Edit Distance

--- src/EditDistance.cpp
+++ src/EditDistance.cpp
@@ -19,7 +19,7 @@ int EditDistance::minDistance(string word1, string word2)
   for (int i = 1; i <= n1; i++) {
     for (int j = 1; j <= n2; j++) {
       if (word1[i - 1] == word2[j - 1])
-        dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j] + 1);
+        dp[i][j] = dp[i - 1][j - 1];
       else
         dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
     }
