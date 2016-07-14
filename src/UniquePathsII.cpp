404d7785b9560a36c588698eb3db2078c98ae27a
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-14 20:35:20 +0800

    Refine Problem 63. Unique Paths II

--- src/UniquePathsII.cpp
+++ src/UniquePathsII.cpp
@@ -4,45 +4,25 @@ int UniquePathsII::uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
 {
   int m = obstacleGrid.size();
 
-  if (m == 0)
-    return 0;
+  if (m == 0) return 0;
 
   int n = obstacleGrid[0].size();
 
-  if (n == 0)
-    return 0;
+  if (n == 0) return 0;
 
-  vector<int> dp(n, 1);
-  dp[0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+  vector<vector<int>> dp(m, vector<int>(n, 0));
+  dp[0][0] = (obstacleGrid[0][0] == 0) ? 1 : 0;
 
-  for (int j = 1; j < n; j++) {
-    if (dp[j - 1] == 0 || obstacleGrid[0][j] == 1)
-      dp[j] = 0;
-    else
-      dp[j] = 1;
-  }
+  for (int i = 1; i < m; i++)
+    dp[i][0] = (dp[i - 1][0] && obstacleGrid[i][0] == 0) ? 1 : 0;
 
-  for (int i = 1; i < m; i++) {
-    if (dp[0] == 0 || obstacleGrid[i][0] == 1)
-      dp[0] = 0;
-    else
-      dp[0] = 1;
+  for (int j = 1; j < n; j++)
+    dp[0][j] = (dp[0][j - 1] && obstacleGrid[0][j] == 0) ? 1 : 0;
 
-    for (int j = 1; j < n; j++) {
-      int up = 0;
+  for (int i = 1; i < m; i++)
+    for (int j = 1; j < n; j++)
+      dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i - 1][j] + dp[i][j - 1];
 
-      if (dp[j] != 0 && obstacleGrid[i][j] != 1)
-        up = dp[j];
-
-      int left = 0;
-
-      if (dp[j - 1] != 0 && obstacleGrid[i][j] != 1)
-        left = dp[j - 1];
-
-      dp[j] = up + left;
-    }
-  }
-
-  return dp[n - 1];
+  return dp[m - 1][n - 1];
 }
 
