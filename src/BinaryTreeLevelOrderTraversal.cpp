a6bda9e7a3d4c7a993b75530b8bcab283b3b1dba
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-16 21:35:34 +0800

    Refine Problem 102. Binary Tree Level Order Traversal

--- src/BinaryTreeLevelOrderTraversal.cpp
+++ src/BinaryTreeLevelOrderTraversal.cpp
@@ -5,53 +5,28 @@ using namespace std;
 
 vector<vector<int>> BinaryTreeLevelOrderTraversal::levelOrder(TreeNode* root)
 {
-  /*
-   * Create a tree like this:
-   *     3
-   *    / \
-   *   9   20
-   *       / \
-   *      15  7
-   */
-  // push 3 (level 1), pop 3 push 9 push 20 (level 2), pop 9, pop 20 push 15 push 7(level 3), pop 15, pop 7
   vector<vector<int>> ret;
+  queue<TreeNode*> q;
 
-  if (root == nullptr)
-    return ret;
+  if (root != nullptr) q.push(root);
 
-  queue<TreeNode*> nodes;
-  nodes.push(root);
-  ret.push_back({});
-  int level = 0;     // this level index
-  int thisLevel = 1; // number of nodes at this level
-  int nextLevel = 0; // number of nodes at next level
+  while (!q.empty()) {
+    int n = q.size();
+    vector<int> newlevel(n);
 
-  while (!nodes.empty()) {
-    TreeNode* front = nodes.front();
+    for (int i = 0; i < n; i++) {
+      TreeNode* current = q.front();
+      q.pop();
+      newlevel[i] = current->val;
 
-    if (thisLevel > 0) {
-      ret[level].push_back(front->val);
-      thisLevel--;
-    } else {
-      thisLevel = nextLevel;
-      nextLevel = 0;
-      level++;
-      ret.push_back({});
-      ret[level].push_back(front->val);
-      thisLevel--;
+      if (current->left != nullptr)
+        q.push(current->left);
+
+      if (current->right != nullptr)
+        q.push(current->right);
     }
 
-    if (front->left != nullptr) {
-      nodes.push(front->left);
-      nextLevel++;
-    }
-
-    if (front->right != nullptr) {
-      nodes.push(front->right);
-      nextLevel++;
-    }
-
-    nodes.pop();
+    ret.push_back(newlevel);
   }
 
   return ret;
