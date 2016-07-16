f8ef22d78f7bd6e43773b2364c68be65a48dc4ed
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-16 23:44:57 +0800

    Refine Problem 104. Maximum Depth of Binary Tree

--- include/MaximumDepthOfBinaryTree.hpp
+++ include/MaximumDepthOfBinaryTree.hpp
@@ -7,6 +7,9 @@ class MaximumDepthOfBinaryTree
 {
 public:
   int maxDepth(TreeNode* root);
+private:
+  int maxDepthRecursive(TreeNode* root);
+  int maxDepthIterative(TreeNode* root);
 };
 
 #endif // MAXIMUM_DEPTH_OF_BINARY_TREE_HPP_
--- src/MaximumDepthOfBinaryTree.cpp
+++ src/MaximumDepthOfBinaryTree.cpp
@@ -1,14 +1,46 @@
 #include "MaximumDepthOfBinaryTree.hpp"
 
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int MaximumDepthOfBinaryTree::maxDepth(TreeNode* root)
 {
+  return maxDepthIterative(root);
+}
+
+int MaximumDepthOfBinaryTree::maxDepthRecursive(TreeNode* root)
+{
   if (root == nullptr)
     return 0;
 
-  int ldepth = maxDepth(root->left);
-  int rdepth = maxDepth(root->right);
-  return max(ldepth, rdepth) + 1;
+  int left = maxDepth(root->left);
+  int right = maxDepth(root->right);
+  return max(left, right) + 1;
+}
+
+int MaximumDepthOfBinaryTree::maxDepthIterative(TreeNode* root)
+{
+  queue<TreeNode*> q;
+  int depth = 0;
+
+  if (root != nullptr) q.push(root);
+
+  while (!q.empty()) {
+    depth++;
+    int n = q.size();
+
+    for (int i = 0; i < n; i++) {
+      TreeNode* node = q.front();
+      q.pop();
+
+      if (node->left != nullptr)
+        q.push(node->left);
+
+      if (node->right != nullptr)
+        q.push(node->right);
+    }
+  }
+
+  return depth;
 }
