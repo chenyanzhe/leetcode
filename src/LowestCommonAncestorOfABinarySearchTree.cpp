ef1bc65de1f64b0e3d9748f9e47f4e949cc71f30
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-16 22:57:13 +0800

    Refine Problem 235. Lowest Common Ancestor of a Binary Search Tree

--- src/LowestCommonAncestorOfABinarySearchTree.cpp
+++ src/LowestCommonAncestorOfABinarySearchTree.cpp
@@ -6,23 +6,11 @@ TreeNode* LowestCommonAncestorOfABinarySearchTree::lowestCommonAncestor(
   if (root == nullptr || p == nullptr || q == nullptr)
     return nullptr;
 
-  if (p->val > q->val)
-    return lowestCommonAncestor(root, q, p);
-
-  if (p->val == q->val)
-    return p;
-
-  if (root->val == p->val)
-    return p;
-
-  if (root->val == q->val)
-    return q;
-
-  if (p->val < root->val && root->val < q->val)
-    return root;
-
-  if (q->val < root->val)
+  if (root->val > p->val && root->val > q->val)
     return lowestCommonAncestor(root->left, p, q);
-  else
+
+  if (root->val < p->val && root->val < q->val)
     return lowestCommonAncestor(root->right, p, q);
+
+  return root;
 }
