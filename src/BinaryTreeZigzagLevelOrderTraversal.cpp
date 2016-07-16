1a2b8a98c352ca9961115ac3a68ca75c26cab873
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-16 21:50:24 +0800

    Refine Problem 103. Binary Tree Zigzag Level Order Traversal

--- src/BinaryTreeZigzagLevelOrderTraversal.cpp
+++ src/BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -1,53 +1,40 @@
 #include "BinaryTreeZigzagLevelOrderTraversal.hpp"
 
 #include <queue>
-#include <algorithm>
 using namespace std;
 
 vector<vector<int>> BinaryTreeZigzagLevelOrderTraversal::zigzagLevelOrder(
                    TreeNode* root)
 {
-  queue<TreeNode*> q;
   vector<vector<int>> ret;
+  queue<TreeNode*> q;
+  bool reverse = false;
 
-  if (root == nullptr)
-    return ret;
-
-  q.push(root);
-  ret.push_back({});
-  int curLevel = 1;
-  int nextLevel = 0;
-  TreeNode* top;
+  if (root != nullptr) q.push(root);
 
   while (!q.empty()) {
-    if (curLevel == 0) {
-      ret.push_back({});
-      curLevel = nextLevel;
-      nextLevel = 0;
+    int n = q.size();
+    vector<int> newlevel(n);
+
+    for (int i = 0; i < n; i++) {
+      TreeNode* current = q.front();
+      q.pop();
+
+      if (reverse)
+        newlevel[n - i - 1] = current->val;
+      else
+        newlevel[i] = current->val;
+
+      if (current->left != nullptr)
+        q.push(current->left);
+
+      if (current->right != nullptr)
+        q.push(current->right);
     }
 
-    top = q.front();
-    q.pop();
-    curLevel--;
-
-    if (top->left != nullptr) {
-      q.push(top->left);
-      nextLevel++;
-    }
-
-    if (top->right != nullptr) {
-      q.push(top->right);
-      nextLevel++;
-    }
-
-    ret.back().push_back(top->val);
+    ret.push_back(newlevel);
+    reverse = !reverse;
   }
-
-  bool r = false;
-
-  for (int i = 0; i < ret.size(); i++, r = !r)
-    if (r)
-      reverse(ret[i].begin(), ret[i].end());
 
   return ret;
 }
