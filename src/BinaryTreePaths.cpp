10b54c5784071e81e00e001ed0c21919ba1b1d9a
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-17 00:26:18 +0800

    Refine Problem 257. Binary Tree Paths

--- src/BinaryTreePaths.cpp
+++ src/BinaryTreePaths.cpp
@@ -3,23 +3,23 @@
 vector<string> BinaryTreePaths::binaryTreePaths(TreeNode* root)
 {
   vector<string> ret;
-  dfs(root, "", ret);
+
+  if (root != nullptr)
+    dfs(root, "", ret);
+
   return ret;
 }
 
 void BinaryTreePaths::dfs(TreeNode* root, string path, vector<string>& ret)
 {
-  if (root == nullptr)
-    return;
+  string val = to_string(root->val);
 
-  if (path.empty())
-    path = to_string(root->val);
-  else
-    path += "->" + to_string(root->val);
+  if (root->left == nullptr && root->right == nullptr)
+    ret.push_back(path + val);
 
-  if (root->left || root->right) {
-    dfs(root->left, path, ret);
-    dfs(root->right, path, ret);
-  } else
-    ret.push_back(path);
+  if (root->left != nullptr)
+    dfs(root->left, path + val + "->", ret);
+
+  if (root->right != nullptr)
+    dfs(root->right, path + val + "->", ret);
 }
