b7906abd34c1ffc056fbbf781c8813324dd498ea
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-17 16:41:13 +0800

    Refine Problem 222. Count Complete Tree Nodes

--- src/CountCompleteTreeNodes.cpp
+++ src/CountCompleteTreeNodes.cpp
@@ -2,33 +2,21 @@
 
 int CountCompleteTreeNodes::countNodes(TreeNode* root)
 {
-  if (root == nullptr)
-    return 0;
+  int hl = 0, hr = 0;
+  TreeNode* l = root, *r = root;
 
-  if (root->left == nullptr)
-    return 1;
-
-  if (root->right == nullptr)
-    return 2;
-
-  TreeNode* ln = root;
-  int ld = 0;
-
-  while (ln != nullptr) {
-    ld++;
-    ln = ln->left;
+  while (l) {
+    hl++;
+    l = l->left;
   }
 
-  TreeNode* rn = root;
-  int rd = 0;
-
-  while (rn != nullptr) {
-    rd++;
-    rn = rn->right;
+  while (r) {
+    hr++;
+    r = r->right;
   }
 
-  if (ld == rd)
-    return (1 << ld) - 1;
+  if (hl == hr)
+    return (1 << hl) - 1;
   else
-    return 1 + countNodes(root->left) + countNodes(root->right);
+    return countNodes(root->left) + countNodes(root->right) + 1;
 }
