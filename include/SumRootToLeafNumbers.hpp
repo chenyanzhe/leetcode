fb22514b6a9b85de5120f11b211c25eb26658504
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-17 19:04:37 +0800

    Refine Problem 129. Sum Root to Leaf Numbers

--- include/SumRootToLeafNumbers.hpp
+++ include/SumRootToLeafNumbers.hpp
@@ -3,17 +3,12 @@
 
 #include "TreeNode.hpp"
 
-#include <vector>
-using namespace std;
-
 class SumRootToLeafNumbers
 {
 public:
   int sumNumbers(TreeNode* root);
 private:
-  void dfs(vector<TreeNode*>& path, vector<vector<int>>& result,
-           TreeNode* start);
-  int getNumberVal(const vector<int>& num);
+  int sumNumbersRec(TreeNode* root, int n);
 };
 
 #endif // SUM_ROOT_TO_LEAF_NUMBERS_HPP_
--- src/SumRootToLeafNumbers.cpp
+++ src/SumRootToLeafNumbers.cpp
@@ -2,54 +2,24 @@
 
 int SumRootToLeafNumbers::sumNumbers(TreeNode* root)
 {
-  vector<TreeNode*> path;
-  vector<vector<int>> result;
-  int ret = 0;
-
   if (root == nullptr)
     return 0;
 
-  dfs(path, result, root);
+  return sumNumbersRec(root, 0);
+}
 
-  for (const auto& num : result)
-    ret += getNumberVal(num);
+int SumRootToLeafNumbers::sumNumbersRec(TreeNode* node, int n)
+{
+  if (node->left == nullptr && node->right == nullptr)
+    return n * 10 + node->val;
+
+  int ret = 0;
+
+  if (node->left != nullptr)
+    ret += sumNumbersRec(node->left, n * 10 + node->val);
+
+  if (node->right != nullptr)
+    ret += sumNumbersRec(node->right, n * 10 + node->val);
 
   return ret;
-}
-
-void SumRootToLeafNumbers::dfs(vector<TreeNode*>& path,
-                               vector<vector<int>>& result, TreeNode* start)
-{
-  if (start->left == nullptr && start->right == nullptr) {
-    vector<int> r;
-
-    for (auto n : path)
-      r.push_back(n->val);
-
-    r.push_back(start->val);
-    result.push_back(r);
-    return;
-  }
-
-  if (start->left != nullptr) {
-    path.push_back(start);
-    dfs(path, result, start->left);
-    path.pop_back();
-  }
-
-  if (start->right != nullptr) {
-    path.push_back(start);
-    dfs(path, result, start->right);
-    path.pop_back();
-  }
-}
-
-int SumRootToLeafNumbers::getNumberVal(const vector<int>& num)
-{
-  int result = 0;
-
-  for (auto d : num)
-    result = result * 10 + d;
-
-  return result;
 }
