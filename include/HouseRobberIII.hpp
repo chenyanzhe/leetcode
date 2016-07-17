787834e778533d3df35548e36e9b1ed421545de5
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-17 20:59:02 +0800

    Problem 337. House Robber III

---
+++ include/HouseRobberIII.hpp
@@ -0,0 +1,19 @@
+#ifndef HOUSE_ROBBER_III_HPP_
+#define HOUSE_ROBBER_III_HPP_
+
+#include "TreeNode.hpp"
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class HouseRobberIII
+{
+public:
+  int rob(TreeNode* root);
+private:
+  int robMem(TreeNode* root, unordered_map<TreeNode*, int>& mem);
+  vector<int> robGreedy(TreeNode* root);
+};
+
+#endif // HOUSE_ROBBER_III_HPP_
---
+++ src/HouseRobberIII.cpp
@@ -0,0 +1,48 @@
+#include "HouseRobberIII.hpp"
+
+#include <algorithm>
+using namespace std;
+
+int HouseRobberIII::rob(TreeNode* root)
+{
+  vector<int> ret = robGreedy(root);
+  return max(ret[0], ret[1]);
+  /*
+    unordered_map<TreeNode*, int> mem;
+    return robMem(root, mem);
+  */
+}
+
+int HouseRobberIII::robMem(TreeNode* root, unordered_map<TreeNode*, int>& mem)
+{
+  if (root == nullptr)
+    return 0;
+
+  if (mem.find(root) != mem.end())
+    return mem[root];
+
+  int val = root->val;
+
+  if (root->left)
+    val += robMem(root->left->left, mem) + robMem(root->left->right, mem);
+
+  if (root->right)
+    val += robMem(root->right->left, mem) + robMem(root->right->right, mem);
+
+  int ret = max(val, robMem(root->left, mem) + robMem(root->right, mem));
+  mem[root] = ret;
+  return ret;
+}
+
+vector<int> HouseRobberIII::robGreedy(TreeNode* root)
+{
+  if (root == nullptr)
+    return vector<int>(2, 0);
+
+  vector<int> ret(2, 0);
+  vector<int> left = robGreedy(root->left);
+  vector<int> right = robGreedy(root->right);
+  ret[0] = max(left[0], left[1]) + max(right[0], right[1]);
+  ret[1] = root->val + left[0] + right[0];
+  return ret;
+}
