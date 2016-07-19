8af06e381f03c63aa4501cde159d91e8751f788a
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-19 19:57:11 +0800

    Refine Problem 96. Unique Binary Search Trees

--- src/UniqueBinarySearchTrees.cpp
+++ src/UniqueBinarySearchTrees.cpp
@@ -5,19 +5,12 @@ using namespace std;
 
 int UniqueBinarySearchTrees::numTrees(int n)
 {
-  vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+  vector<int> dp(n + 1, 0);
+  dp[0] = 1;
 
-  for (int i = 1; i <= n + 1; i++)
-    dp[i][i] = dp[i][i - 1] = 1;
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= i; j++)
+      dp[i] += dp[j - 1] * dp[i - j];
 
-  for (int l = 2; l <= n; l++) {
-    for (int i = 1; i <= n + 1 - l; i++) {
-      int j = i + l - 1;
-
-      for (int k = i; k <= j; k++)
-        dp[i][j] += dp[i][k - 1] * dp[k + 1][j];
-    }
-  }
-
-  return dp[1][n];
+  return dp[n];
 }
