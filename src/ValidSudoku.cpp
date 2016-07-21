eec6e93247a9627a262dfefff302930cdd1454f1
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-21 22:56:42 +0800

    Refine Problem 36. Valid Sudoku

--- src/ValidSudoku.cpp
+++ src/ValidSudoku.cpp
@@ -4,8 +4,8 @@ bool ValidSudoku::isValidSudoku(vector<vector<char>>& board)
 {
   bool used1[9][9] = {false}, used2[9][9] = {false}, used3[9][9] = {false};
 
-  for(int i = 0; i < board.size(); i++)
-    for(int j = 0; j < board[i].size(); j++)
+  for (int i = 0; i < board.size(); i++)
+    for (int j = 0; j < board[i].size(); j++)
       if (board[i][j] != '.') {
         int num = board[i][j] - '0' - 1;
         int k = i / 3 * 3 + j / 3;
