466ad80c8d747a6d18b80f4a29424d181e3be50b
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-21 21:09:55 +0800

    Problem 349. Intersection of Two Arrays

---
+++ include/IntersectionOfTwoArrays.hpp
@@ -0,0 +1,13 @@
+#ifndef INTERSECTION_OF_TWO_ARRAYS_HPP_
+#define INTERSECTION_OF_TWO_ARRAYS_HPP_
+
+#include <vector>
+using namespace std;
+
+class IntersectionOfTwoArrays
+{
+public:
+  vector<int> intersection(vector<int>& nums1, vector<int>& nums2);
+};
+
+#endif // INTERSECTION_OF_TWO_ARRAYS_HPP_
