6107e2c3e2bd7869ecdc4030e9521030acc306d5
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-07-23 15:46:00 +0800

    Refine Problem 274. H-Index

--- include/HIndex.hpp
+++ include/HIndex.hpp
@@ -7,8 +7,12 @@ using namespace std;
 
 class HIndex {
 public:
-
     int hIndex(vector<int> &citations);
+
+private:
+    int hIndexSort(vector<int> &citations);
+
+    int hIndexHashTable(vector<int> &citations);
 };
 
 #endif // H_INDEX_HPP_
--- src/HIndex.cpp
+++ src/HIndex.cpp
@@ -1,8 +1,19 @@
 #include "HIndex.hpp"
 
-using namespace std;
-
 int HIndex::hIndex(vector<int> &citations) {
+    return hIndexHashTable(citations);
+}
+
+int HIndex::hIndexSort(vector<int> &citations) {
+    sort(citations.begin(), citations.end());
+    int n = citations.size();
+    int i = n - 1;
+    while (i >= 0 && citations[i] >= n - i)
+        i--;
+    return n - i - 1;
+}
+
+int HIndex::hIndexHashTable(vector<int> &citations) {
     int n = citations.size();
     vector<int> count(n + 1, 0);
 
