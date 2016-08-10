3a8ebaeb467fcbcfd8d337552c71905285b9a66a
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-08-10 21:53:15 +0800

    Refine Problem 88. Merge Sorted Array

--- src/MergeSortedArray.cpp
+++ src/MergeSortedArray.cpp
@@ -2,26 +2,15 @@
 
 void MergeSortedArray::merge(vector<int> &nums1, int m, vector<int> &nums2,
                              int n) {
-    vector<int> done;
-    done.reserve(m + n);
-    int i, j, k;
-
-    for (i = 0, j = 0, k = 0; i < m + n; i++) {
-        if (j == m)
-            done[i] = nums2[k++];
-        else if (k == n)
-            done[i] = nums1[j++];
-        else {
-            int v1 = nums1[j];
-            int v2 = nums2[k];
-
-            if (v1 < v2)
-                done[i] = nums1[j++];
-            else
-                done[i] = nums2[k++];
+    int i = m - 1;
+    int j = n - 1;
+    for (int k = m + n - 1; k >= 0; k--) {
+        if (i >= 0 && j >= 0) {
+            nums1[k] = (nums1[i] >= nums2[j]) ? nums1[i--] : nums2[j--];
+        } else if (i >= 0) {
+            nums1[k] = nums1[i--];
+        } else {
+            nums1[k] = nums2[j--];
         }
     }
-
-    for (i = 0; i < m + n; i++)
-        nums1[i] = done[i];
 }
