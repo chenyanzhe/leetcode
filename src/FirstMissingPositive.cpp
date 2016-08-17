a5c5eac895571aad6a0895b7e84a42fb4fa230b5
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-08-17 21:51:48 +0800

    Refine Problem 41. First Missing Positive

--- src/FirstMissingPositive.cpp
+++ src/FirstMissingPositive.cpp
@@ -6,8 +6,12 @@ int FirstMissingPositive::firstMissingPositive(vector<int> &nums) {
     int n = nums.size();
 
     for (int i = 0; i < n; i++) {
-        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
-            swap(nums[nums[i] - 1], nums[i]);
+        int t = nums[i];
+        while (1 <= t && t <= n && nums[t - 1] != t) {
+            int nt = nums[t - 1];
+            nums[t - 1] = t;
+            t = nt;
+        }
     }
 
     for (int i = 0; i < n; i++) {
