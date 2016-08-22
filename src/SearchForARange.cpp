856ea227fed96824f76e4384980c4d648b60a4c5
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-08-22 10:25:42 +0800

    Refine Problem 34. Search for a Range

--- src/SearchForARange.cpp
+++ src/SearchForARange.cpp
@@ -2,41 +2,53 @@
 
 vector<int> SearchForARange::searchRange(vector<int> &nums, int target) {
     vector<int> ret{-1, -1};
-    int sz = nums.size();
 
-    if (sz == 0)
-        return ret;
+    if (nums.empty()) return ret;
 
+    int len = nums.size();
     int i = 0;
-    int j = sz - 1;
+    int j = len - 1;
 
-    while (i < j) {
-        int mid = (i + j) / 2;
-
-        if (nums[mid] < target)
-            i = mid + 1;
-        else
-            j = mid;
+    while (i < j - 1) {
+        int m = i + (j - i) / 2;
+        if (target == nums[m]) {
+            j = m;
+        } else if (target > nums[m]) {
+            i = m;
+        } else {
+            j = m;
+        }
     }
 
-    if (nums[i] == target)
+    if (nums[i] == target) {
         ret[0] = i;
-    else
+    } else if (nums[j] == target) {
+        ret[0] = j;
+    } else {
         return ret;
-
-    j = sz - 1;
-
-    while (i < j) {
-        int mid = (i + j) / 2 + 1;
-
-        if (nums[mid] > target)
-            j = mid - 1;
-        else
-            i = mid;
     }
 
-    if (nums[i] == target)
+    i = 0;
+    j = len - 1;
+
+    while (i < j - 1) {
+        int m = i + (j - i) / 2;
+        if (target == nums[m]) {
+            i = m;
+        } else if (target > nums[m]) {
+            i = m;
+        } else {
+            j = m;
+        }
+    }
+
+    if (nums[j] == target) {
+        ret[1] = j;
+    } else if (nums[i] == target) {
         ret[1] = i;
+    } else {
+        return ret;
+    }
 
     return ret;
 }
