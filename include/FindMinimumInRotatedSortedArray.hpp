aaa4bac6cb0d5635001d9a1414e2f77b2ee1ce0c
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-08-23 10:01:03 +0800

    Refine Problem 153. Find Minimum in Rotated Sorted Array

--- include/FindMinimumInRotatedSortedArray.hpp
+++ include/FindMinimumInRotatedSortedArray.hpp
@@ -8,9 +8,6 @@ using namespace std;
 class FindMinimumInRotatedSortedArray {
 public:
     int findMin(vector<int> &nums);
-
-private:
-    int findMin(vector<int> &nums, int begin, int end);
 };
 
 #endif // FIND_MINIMUM_IN_ROTATED_SORTED_ARRAY_HPP_
--- src/FindMinimumInRotatedSortedArray.cpp
+++ src/FindMinimumInRotatedSortedArray.cpp
@@ -1,24 +1,16 @@
 #include "FindMinimumInRotatedSortedArray.hpp"
 
 int FindMinimumInRotatedSortedArray::findMin(vector<int> &nums) {
-    return findMin(nums, 0, nums.size() - 1);
-}
+    int l = 0, r = nums.size() - 1;
 
-int FindMinimumInRotatedSortedArray::findMin(vector<int> &nums, int begin,
-                                             int end) {
-    if (nums[begin] < nums[end])
-        return nums[begin];
+    while (nums[l] > nums[r]) {
+        int m = l + (r - l) / 2;
 
-    if (begin == end)
-        return nums[begin];
+        if (nums[m] >= nums[l] && nums[m] > nums[r])
+            l = m + 1;
+        else
+            r = m;
+    }
 
-    if (begin + 1 == end)
-        return nums[end];
-
-    int mid = (begin + end) / 2;
-
-    if (nums[begin] > nums[mid])
-        return findMin(nums, begin, mid);
-    else   // nums[mid] > nums[end]
-        return findMin(nums, mid, end);
+    return nums[l];
 }
