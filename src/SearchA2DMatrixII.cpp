c9ff735461871a8749d4b34363ef6d0a8b710aac
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-08-24 21:40:13 +0800

    Refine Problem 240. Search a 2D Matrix II

--- src/SearchA2DMatrixII.cpp
+++ src/SearchA2DMatrixII.cpp
@@ -1,12 +1,16 @@
 #include "SearchA2DMatrixII.hpp"
 
 bool SearchA2DMatrixII::searchMatrix(vector<vector<int>> &matrix, int target) {
-    int r = matrix.size();
-    int c = matrix[0].size();
-    int i = 0;
-    int j = c - 1;
+    int m = matrix.size();
+    if (m == 0) return false;
+    int n = matrix[0].size();
+    if (n == 0) return false;
 
-    while (i < r && j >= 0) {
+    int i = 0;
+    int j = n - 1;
+
+    // from top right to bottom left
+    while (i < m && j >= 0) {
         int p = matrix[i][j];
 
         if (p == target)
