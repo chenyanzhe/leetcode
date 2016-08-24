f866675ac72ebc43ad36b94dff99409d712751c3
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-08-24 22:04:24 +0800

    Refine Problem 162. Find Peak Element

--- include/FindPeakElement.hpp
+++ include/FindPeakElement.hpp
@@ -8,9 +8,6 @@ using namespace std;
 class FindPeakElement {
 public:
     int findPeakElement(vector<int> &nums);
-
-private:
-    int findPeakElement(vector<int> &nums, int begin, int end);
 };
 
 #endif // FIND_PEAK_ELEMENT_HPP_
--- src/FindPeakElement.cpp
+++ src/FindPeakElement.cpp
@@ -1,35 +1,17 @@
 #include "FindPeakElement.hpp"
 
 int FindPeakElement::findPeakElement(vector<int> &nums) {
-    if (nums.empty())
-        return -1;
+    int l = 0, r = nums.size() - 1;
 
-    return findPeakElement(nums, 0, nums.size() - 1);
-}
+    while (l + 1 < r) {
+        int m = l + (r - l) / 2;
+        if (nums[m - 1] > nums[m])
+            r = m;
+        else if (nums[m] < nums[m + 1])
+            l = m;
+        else
+            r = m;
+    }
 
-int FindPeakElement::findPeakElement(vector<int> &nums, int begin, int end) {
-    if (begin == end)
-        return begin;
-
-    if (begin + 1 == end)
-        return nums[begin] < nums[end] ? end : begin;
-
-    // if nums[begin:end] does not contain *real* peak
-    // case 1. strict increasing or decreasing order
-    // case 2. forms a big "V"
-    if (nums[begin] > nums[begin + 1])
-        return begin;
-
-    if (nums[end] > nums[end - 1])
-        return end;
-
-    int mid = (begin + end) / 2;
-
-    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
-        return mid;
-
-    if (nums[mid] > nums[mid - 1])
-        return findPeakElement(nums, mid + 1, end);
-    else
-        return findPeakElement(nums, begin, mid - 1);
+    return nums[l] < nums[r] ? r : l;
 }
