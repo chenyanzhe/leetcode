a73d8a2edcf40e3d7654c82761119424ad6d93bf
Yanzhe Chen <t-yachen@microsoft.com>
2016-08-25 14:42:11 +0800

    Refine Problem 349. Intersection of Two Arrays

--- include/IntersectionOfTwoArrays.hpp
+++ include/IntersectionOfTwoArrays.hpp
@@ -8,6 +8,13 @@ using namespace std;
 class IntersectionOfTwoArrays {
 public:
     vector<int> intersection(vector<int> &nums1, vector<int> &nums2);
+
+private:
+    vector<int> intersection_HashMap(vector<int> &nums1, vector<int> &nums2);
+
+    vector<int> intersection_BinarySearch(vector<int> &nums1, vector<int> &nums2);
+
+    bool binarySearch(vector<int> &nums, int target);
 };
 
 #endif // INTERSECTION_OF_TWO_ARRAYS_HPP_
--- src/IntersectionOfTwoArrays.cpp
+++ src/IntersectionOfTwoArrays.cpp
@@ -1,11 +1,15 @@
 #include "IntersectionOfTwoArrays.hpp"
 
 #include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> IntersectionOfTwoArrays::intersection(vector<int> &nums1,
-                                                  vector<int> &nums2) {
+vector<int> IntersectionOfTwoArrays::intersection(vector<int> &nums1, vector<int> &nums2) {
+    return intersection_BinarySearch(nums1, nums2);
+}
+
+vector<int> IntersectionOfTwoArrays::intersection_HashMap(vector<int> &nums1, vector<int> &nums2) {
     unordered_set<int> s1, s2;
     vector<int> ret;
 
@@ -20,4 +24,37 @@ vector<int> IntersectionOfTwoArrays::intersection(vector<int> &nums1,
         ret.push_back(d);
 
     return ret;
+}
+
+vector<int> IntersectionOfTwoArrays::intersection_BinarySearch(vector<int> &nums1, vector<int> &nums2) {
+    sort(nums2.begin(), nums2.end());
+
+    unordered_set<int> set;
+    for (int i = 0; i < nums1.size(); i++) {
+        if (binarySearch(nums2, nums1[i]))
+            set.insert(nums1[i]);
+    }
+
+    vector<int> ret;
+    for (auto d : set)
+        ret.push_back(d);
+
+    return ret;
+}
+
+bool IntersectionOfTwoArrays::binarySearch(vector<int> &nums, int target) {
+    if (nums.empty()) return false;
+
+    int l = 0, r = nums.size() - 1;
+    while (l + 1 < r) {
+        int m = l + (r - l) / 2;
+        if (nums[m] == target)
+            return true;
+        if (nums[m] < target)
+            l = m;
+        else
+            r = m;
+    }
+
+    return nums[l] == target || nums[r] == target;
 }
