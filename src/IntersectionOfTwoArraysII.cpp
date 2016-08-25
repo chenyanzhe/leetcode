5f523b187c3f0c43b42ef04e936bba17aa275932
Yanzhe Chen <t-yachen@microsoft.com>
2016-08-25 15:33:31 +0800

    Refine Problem 350. Intersection of Two Arrays II

--- src/IntersectionOfTwoArraysII.cpp
+++ src/IntersectionOfTwoArraysII.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-vector<int> IntersectionOfTwoArraysII::intersection(vector<int> &nums1,
-                                                    vector<int> &nums2) {
+vector<int> IntersectionOfTwoArraysII::intersection(vector<int> &nums1, vector<int> &nums2) {
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
     int i = 0;
