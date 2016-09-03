b8273de0d7d32ed2390f30c53799f52a473c9855
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-03 23:29:06 +0800

    Refine Problem 2. Add Two Numbers

--- src/AddTwoNumbers.cpp
+++ src/AddTwoNumbers.cpp
@@ -1,27 +1,23 @@
 #include "AddTwoNumbers.hpp"
 
-#include <cstdlib>
-
 ListNode *AddTwoNumbers::addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode stackAnchor(0);
-    ListNode *tail = &stackAnchor;
-    div_t sum = {0, 0};
-
-    while (sum.quot > 0 || l1 || l2) {
+    ListNode dummyNode(0);
+    ListNode *tail = &dummyNode;
+    int carry = 0;
+    while (l1 || l2 || carry) {
+        int sum = carry;
         if (l1) {
-            sum.quot += l1->val;
+            sum += l1->val;
             l1 = l1->next;
         }
-
         if (l2) {
-            sum.quot += l2->val;
+            sum += l2->val;
             l2 = l2->next;
         }
-
-        sum = div(sum.quot, 10);
-        tail->next = new ListNode(sum.rem);
+        carry = sum / 10;
+        sum = sum % 10;
+        tail->next = new ListNode(sum);
         tail = tail->next;
     }
-
-    return stackAnchor.next;
+    return dummyNode.next;
 }
