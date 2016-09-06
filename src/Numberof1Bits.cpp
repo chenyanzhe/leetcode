f30349be49c7428a3e6fd4ec73daad6862aa94da
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-06 23:46:00 +0800

    Refine Problem 191. Number of 1 Bits

--- src/Numberof1Bits.cpp
+++ src/Numberof1Bits.cpp
@@ -1,12 +1,10 @@
 #include "Numberof1Bits.hpp"
 
 int Numberof1Bits::hammingWeight(uint32_t n) {
-    int result = 0;
-
-    while (n != 0) {
-        result += n & 1;
-        n >>= 1;
+    int ret = 0;
+    while (n) {
+        n &= (n - 1);
+        ret++;
     }
-
-    return result;
+    return ret;
 }
