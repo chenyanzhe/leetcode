9efc5eb19e072c6342265c74a33cc5dbfd0298ce
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-07 23:02:10 +0800

    Refine Problem 273. Integer to English Words

--- src/IntegerToEnglishWords.cpp
+++ src/IntegerToEnglishWords.cpp
@@ -1,59 +1,23 @@
 #include "IntegerToEnglishWords.hpp"
 
 string IntegerToEnglishWords::numberToWords(int num) {
-    if (num == 0)
-        return "Zero";
-
-    string ret = "";
-    int ib = 0;
-
-    while (num != 0) {
-        int base = num % 1000;
-
-        if (base > 0) {
-            if (ib == 0)
-                ret = parse(base);
-            else
-                ret = parse(base) + " " + dict_b[ib] + " " + ret;
-        }
-
-        ib++;
+    string res = parse(num % 1000);
+    vector<string> v = {"Thousand", "Million", "Billion"};
+    for (int i = 0; i < 3; ++i) {
         num /= 1000;
+        res = num % 1000 ? parse(num % 1000) + " " + v[i] + " " + res : res;
     }
-
-    // strip the trailing space
-    if (ret[ret.size() - 1] == ' ')
-        ret = ret.substr(0, ret.size() - 1);
-
-    return ret;
+    while (res.back() == ' ') res.pop_back();
+    return res.empty() ? "Zero" : res;
 }
 
 string IntegerToEnglishWords::parse(int value) {
-    string ret = "";
-
-    if (value >= 100)
-        ret = dict_a[value / 100] + " Hundred";
-
-    value %= 100;
-
-    if (value == 0)
-        return ret;
-    else if (value <= 20)
-        ret += " " + dict_a[value];
-    else {
-        int upper = 29;
-
-        while (value > upper)
-            upper += 10;
-
-        ret += " " + dict_a[upper / 10 * 10];
-
-        if (value - upper / 10 * 10 > 0)
-            ret += " " + dict_a[value - upper / 10 * 10];
-    }
-
-    if (ret[0] == ' ')
-        ret = ret.substr(1);
-
-    return ret;
+    vector<string> v1 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+                         "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> v2 = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    string res;
+    int a = value / 100, b = value % 100, c = value % 10;
+    res = b < 20 ? v1[b] : v2[b / 10] + (c ? " " + v1[c] : "");
+    if (a > 0) res = v1[a] + " Hundred" + (b ? " " + res : "");
+    return res;
 }
