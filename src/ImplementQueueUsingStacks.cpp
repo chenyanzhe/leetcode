492233d91901ba048782305050d5674678b78b1c
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-07 20:58:35 +0800

    Refine Problem 232. Implement Queue using Stacks

--- src/ImplementQueueUsingStacks.cpp
+++ src/ImplementQueueUsingStacks.cpp
@@ -1,31 +1,32 @@
 #include "ImplementQueueUsingStacks.hpp"
 
 void ImplementQueueUsingStacks::push(int x) {
-    while (!primary.empty()) {
-        backup.push(primary.top());
-        primary.pop();
-    }
-
-    primary.push(x);
-
-    while (!backup.empty()) {
-        primary.push(backup.top());
-        backup.pop();
-    }
+    backup.push(x);
 }
 
 void ImplementQueueUsingStacks::pop(void) {
+    if (primary.empty()) {
+        while (!backup.empty()) {
+            primary.push(backup.top());
+            backup.pop();
+        }
+    }
     if (!primary.empty())
         primary.pop();
 }
 
 int ImplementQueueUsingStacks::peek(void) {
+    if (primary.empty()) {
+        while (!backup.empty()) {
+            primary.push(backup.top());
+            backup.pop();
+        }
+    }
     if (!primary.empty())
         return primary.top();
-
     return 0;
 }
 
 bool ImplementQueueUsingStacks::empty(void) {
-    return primary.empty();
+    return backup.empty() && primary.empty();
 }
