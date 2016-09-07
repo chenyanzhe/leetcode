65e89f6a9488b7b5ca8c5292f30edc52ddd23e41
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-07 14:49:00 +0800

    Refine Problem 215. Kth Largest Element in an Array

--- include/KthLargestElementInAnArray.hpp
+++ include/KthLargestElementInAnArray.hpp
@@ -8,11 +8,6 @@ using namespace std;
 class KthLargestElementInAnArray {
 public:
     int findKthLargest(vector<int> &nums, int k);
-
-private:
-    int findKthLargest(vector<int> &nums, int i, int j, int k);
-
-    int partition(vector<int> &nums, int begin, int end);
 };
 
 #endif // KTH_LARGEST_ELEMENT_IN_AN_ARRAY_HPP_
--- src/KthLargestElementInAnArray.cpp
+++ src/KthLargestElementInAnArray.cpp
@@ -1,39 +1,27 @@
 #include "KthLargestElementInAnArray.hpp"
 
+#include <queue>
+
+using namespace std;
+
 int KthLargestElementInAnArray::findKthLargest(vector<int> &nums, int k) {
-    return findKthLargest(nums, 0, nums.size() - 1, k);
-}
+    struct MinOrder {
+        bool operator()(const int &a, const int &b) {
+            return a > b;
+        }
+    };
 
-int KthLargestElementInAnArray::findKthLargest(vector<int> &nums, int i, int j,
-                                               int k) {
-    if (i == j)
-        return nums[i];
+    typedef priority_queue<int, vector<int>, MinOrder> MinHeap;
+    MinHeap h;
 
-    int p = partition(nums, i, j);
-
-    if (p - i == k - 1)
-        return nums[p];
-    else if (p - i < k - 1)
-        return findKthLargest(nums, p + 1, j, k - (p - i + 1));
-    else
-        return findKthLargest(nums, i, p - 1, k);
-}
-
-int KthLargestElementInAnArray::partition(vector<int> &nums, int begin,
-                                          int end) {
-    int i = begin;
-
-    for (int j = begin + 1; j <= end; j++) {
-        if (nums[j] <= nums[i])
-            continue;
-        else {
-            int temp = nums[j];
-            nums[j] = nums[i + 1];
-            nums[i + 1] = nums[i];
-            nums[i] = temp;
-            i++;
+    for (int i = 0; i < k; i++)
+        h.push(nums[i]);
+    for (int i = k; i < nums.size(); i++) {
+        if (nums[i] > h.top()) {
+            h.pop();
+            h.push(nums[i]);
         }
     }
 
-    return i;
+    return h.top();
 }
