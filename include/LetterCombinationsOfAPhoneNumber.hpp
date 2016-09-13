b219be6c1825c095394741d5c913357db37f1b61
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-13 21:36:11 +0800

    Refine Problem 17. Letter Combinations of a Phone Number

--- include/LetterCombinationsOfAPhoneNumber.hpp
+++ include/LetterCombinationsOfAPhoneNumber.hpp
@@ -3,12 +3,30 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class LetterCombinationsOfAPhoneNumber {
+private:
+    unordered_map<int, string> dict{
+            {0, ""},
+            {1, ""},
+            {2, "abc"},
+            {3, "def"},
+            {4, "ghi"},
+            {5, "jkl"},
+            {6, "mno"},
+            {7, "pqrs"},
+            {8, "tuv"},
+            {9, "wxyz"}
+    };
+
 public:
     vector<string> letterCombinations(string digits);
+
+private:
+    void backtrack(string &digits, int begin, string &current, vector<string> &result);
 };
 
 #endif // LETTER_COMBINATIONS_OF_A_PHONE_NUMBER_HPP_
--- src/LetterCombinationsOfAPhoneNumber.cpp
+++ src/LetterCombinationsOfAPhoneNumber.cpp
@@ -1,34 +1,22 @@
 #include "LetterCombinationsOfAPhoneNumber.hpp"
 
-#include <queue>
+vector<string> LetterCombinationsOfAPhoneNumber::letterCombinations(string digits) {
+    vector<string> result;
+    if (digits.empty()) return result;
 
-using namespace std;
+    string current;
+    backtrack(digits, 0, current, result);
+    return result;
+}
 
-vector<string> LetterCombinationsOfAPhoneNumber::letterCombinations(
-        string digits) {
-    vector<string> mapping{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    queue<string> q;
-    q.push("");
-
-    for (int i = 0; i < digits.size(); i++) {
-        while (q.front().size() == i) {
-            string t = q.front();
-            q.pop();
-
-            for (auto c : mapping[digits[i] - '0'])
-                q.push(t + string(1, c));
-        }
+void LetterCombinationsOfAPhoneNumber::backtrack(string &digits, int begin, string &current, vector<string> &result) {
+    if (begin >= digits.size()) {
+        result.push_back(current);
+        return;
     }
-
-    vector<string> res;
-
-    if (digits.size() == 0)
-        return res;
-
-    while (!q.empty()) {
-        res.push_back(q.front());
-        q.pop();
+    for (auto c : dict[digits[begin] - '0']) {
+        current.push_back(c);
+        backtrack(digits, begin + 1, current, result);
+        current.pop_back();
     }
-
-    return res;
 }
