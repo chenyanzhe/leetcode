c3b9a244944252980daef96ceb3a56b76cd83c35
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-09-17 23:07:58 +0800

    Refine Problem 69. Sqrt(x)

--- src/SqrtX.cpp
+++ src/SqrtX.cpp
@@ -5,15 +5,17 @@
 using namespace std;
 
 int SqrtX::mySqrt(int x) {
-    int64_t l = 1, r = x;
-
-    while (l + 1 < r) {
-        int64_t m = l + (r - l) / 2;
-        if (m * m < x)
+    if (x <= 0) return 0;
+    int64_t l = 1;
+    int64_t r = x;
+    while (l < r) {
+        int64_t m = l + (r - l + 1) / 2;
+        if (m * m == (int64_t) x)
+            return (int) m;
+        if (m * m < (int64_t) x)
             l = m;
         else
-            r = m;
+            r = m - 1;
     }
-
-    return r * r <= x ? r : l;
+    return (int) l;
 }
