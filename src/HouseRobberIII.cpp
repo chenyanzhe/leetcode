e473789058c1be9f701b3a1339943c4571f2abd7
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-13 14:31:08 +0800

    Refine Problem 337. House Robber III

--- src/HouseRobberIII.cpp
+++ src/HouseRobberIII.cpp
@@ -11,6 +11,7 @@ int HouseRobberIII::rob(TreeNode *root) {
     */
 }
 
+// Recursive + Memoization
 int HouseRobberIII::robMem(TreeNode *root, unordered_map<TreeNode *, int> &mem) {
     if (root == nullptr)
         return 0;
@@ -35,10 +36,14 @@ vector<int> HouseRobberIII::robGreedy(TreeNode *root) {
     if (root == nullptr)
         return vector<int>(2, 0);
 
+    // first element: root is not robbed
+    // second element: root is robbed
     vector<int> ret(2, 0);
     vector<int> left = robGreedy(root->left);
     vector<int> right = robGreedy(root->right);
+    // if root is not robbed, feel free to rob its subtree
     ret[0] = max(left[0], left[1]) + max(right[0], right[1]);
+    // if root is robbed, subtree's root cannot be robbed
     ret[1] = root->val + left[0] + right[0];
     return ret;
 }
