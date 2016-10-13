ba363fc800f163d6d9ac4b7ed4cea72904ab8f71
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-13 14:18:16 +0800

    Refine Problem 404. Sum of Left Leaves

--- include/SumOfLeftLeaves.hpp
+++ include/SumOfLeftLeaves.hpp
@@ -6,6 +6,9 @@
 class SumOfLeftLeaves {
 public:
     int sumOfLeftLeaves(TreeNode *root);
+private:
+    int sumOfLeftLeaves_Recursive(TreeNode *root);
+    int sumOfLeftLeaves_Iterative(TreeNode *root);
 };
 
 #endif // SUM_OF_LEFT_LEAVES_HPP_
--- src/SumOfLeftLeaves.cpp
+++ src/SumOfLeftLeaves.cpp
@@ -1,35 +1,44 @@
 #include "SumOfLeftLeaves.hpp"
 
-#include <queue>
+#include <stack>
 
 using namespace std;
 
 int SumOfLeftLeaves::sumOfLeftLeaves(TreeNode *root) {
-    queue<TreeNode *> left;
-    queue<TreeNode *> right;
+    return sumOfLeftLeaves_Recursive(root);
+}
 
-    int ret = 0;
-    if (root) right.push(root);
+int SumOfLeftLeaves::sumOfLeftLeaves_Recursive(TreeNode *root) {
+    if (root == nullptr) return 0;
+    int ans = 0;
+    if (root->left) {
+        if (root->left->left == nullptr && root->left->right == nullptr)
+            ans += root->left->val;
+        else
+            ans += sumOfLeftLeaves_Recursive(root->left);
+    }
+    if (root->right)
+        ans += sumOfLeftLeaves_Recursive(root->right);
+    return ans;
+}
 
-    while (!left.empty() || !right.empty()) {
-        int ln = left.size();
-        int rn = right.size();
-        while (ln--) {
-            TreeNode *n = left.front();
-            left.pop();
-            if (n->left || n->right) {
-                if (n->left) left.push(n->left);
-                if (n->right) right.push(n->right);
-            } else
-                ret += n->val;
+int SumOfLeftLeaves::sumOfLeftLeaves_Iterative(TreeNode *root) {
+    if (root == nullptr) return 0;
+    int ans = 0;
+    stack<TreeNode *> s;
+    s.push(root);
+    while (!s.empty()) {
+        TreeNode *n = s.top();
+        if (n->left) {
+            if (n->left->left == nullptr && n->left->right == nullptr)
+                ans += n->left->val;
+            else
+                s.push(n->left);
         }
-        while (rn--) {
-            TreeNode *n = right.front();
-            right.pop();
-            if (n->left) left.push(n->left);
-            if (n->right) right.push(n->right);
+        if (n->right) {
+            if (n->right->left || n->right->right)
+                s.push(n->right);
         }
     }
-
-    return ret;
+    return ans;
 }
