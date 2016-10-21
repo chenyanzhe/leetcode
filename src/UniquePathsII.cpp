dab34209e65f389141bda82a58d7db2b8c1ec507
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-21 20:58:52 +0800

    Refine Problem 63. Unique Paths II

--- src/UniquePathsII.cpp
+++ src/UniquePathsII.cpp
@@ -13,10 +13,10 @@ int UniquePathsII::uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
     dp[0][0] = (obstacleGrid[0][0] == 0) ? 1 : 0;
 
     for (int i = 1; i < m; i++)
-        dp[i][0] = (dp[i - 1][0] && obstacleGrid[i][0] == 0) ? 1 : 0;
+        dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : dp[i - 1][0];
 
     for (int j = 1; j < n; j++)
-        dp[0][j] = (dp[0][j - 1] && obstacleGrid[0][j] == 0) ? 1 : 0;
+        dp[0][j] = obstacleGrid[0][j] == 1 ? 0 : dp[0][j - 1];
 
     for (int i = 1; i < m; i++)
         for (int j = 1; j < n; j++)
