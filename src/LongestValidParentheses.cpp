3f82de5c578b9ccd91f7bd71c8388de742258df8
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-21 20:44:08 +0800

    Refine Problem 32. Longest Valid Parentheses

--- src/LongestValidParentheses.cpp
+++ src/LongestValidParentheses.cpp
@@ -5,22 +5,19 @@
 using namespace std;
 
 int LongestValidParentheses::longestValidParentheses(string s) {
-    int n = s.size();
-    vector<int> dp(n + 1, 0);
-
-    for (int i = 2; i <= n; i++) {
-        if (s[i - 2] == '(' && s[i - 1] == ')')
-            dp[i] = dp[i - 2] + 2;
-
-        if (s[i - 1] == ')' && i - dp[i - 1] - 2 >= 0
-            && s[i - dp[i - 1] - 2] == '(')
-            dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
-    }
-
+    int len = s.size();
+    vector<int> dp(len, 0);
     int ret = 0;
 
-    for (int i = 2; i <= n; i++)
+    for (int i = 1; i < len; i++) {
+        if (s[i] == '(') continue;
+        if (s[i - 1] == '(')
+            dp[i] = (i == 1 ? 2 : dp[i - 2] + 2);
+        else if (i - dp[i - 1] - 1 >= 0 && s[i - dp[i - 1] - 1] == '(')
+            dp[i] = dp[i - 1] + 2 + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0);
+
         ret = max(ret, dp[i]);
+    }
 
     return ret;
 }
