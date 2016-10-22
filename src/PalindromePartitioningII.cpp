e68aec6ebfb979171dda06e677144fec931b0a48
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-22 16:00:14 +0800

    Refine Problem 132. Palindrome Partitioning II

--- src/PalindromePartitioningII.cpp
+++ src/PalindromePartitioningII.cpp
@@ -5,18 +5,27 @@
 using namespace std;
 
 int PalindromePartitioningII::minCut(string s) {
+    if (s.empty()) return 0;
+
     int n = s.size();
     vector<int> minCuts(n + 1, 0);
-    vector<vector<bool>> isPalin(n, vector<bool>(n, false));
+    vector<vector<bool>> dict(n, vector<bool>(n, false));
 
-    for (int i = 0; i <= n; i++)
-        minCuts[i] = i - 1;
+    for (int l = 1; l <= n; l++) {
+        for (int i = 0; i + l <= n; i++) {
+            int j = i + l - 1;
+            if (s[i] == s[j] && (l <= 3 || dict[i + 1][j - 1]))
+                dict[i][j] = true;
+        }
+    }
 
-    for (int j = 1; j < n; j++) {
-        for (int i = j; i >= 0; i--) {
-            if (s[i] == s[j] && (i + 1 == j || i == j || isPalin[i + 1][j - 1])) {
-                isPalin[i][j] = true;
-                minCuts[j + 1] = min(minCuts[j + 1], minCuts[i] + 1);
+    for (int i = 0; i < n; i++) {
+        if (dict[0][i]) minCuts[i + 1] = 0;
+        else {
+            minCuts[i + 1] = i;
+            for (int j = 1; j <= i; j++) {
+                if (dict[j][i])
+                    minCuts[i + 1] = min(minCuts[i + 1], minCuts[j] + 1);
             }
         }
     }
