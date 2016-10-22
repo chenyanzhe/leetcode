e97f9fbb96fb2a03ae81bffe988e1f5d3c3f6143
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-22 19:39:23 +0800

    Refine Problem 279. Perfect Squares

--- src/PerfectSquares.cpp
+++ src/PerfectSquares.cpp
@@ -5,16 +5,12 @@
 using namespace std;
 
 int PerfectSquares::numSquares(int n) {
-    vector<int> dp(n + 1, 0);
+    vector<int> dp(n + 1, n);
 
-    for (int i = 1; i <= n; i++) {
-        dp[i] = dp[i - 1];
-
-        for (int j = 2; j * j <= i; j++)
-            dp[i] = min(dp[i], dp[i - j * j]);
-
-        dp[i] += 1;
-    }
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j * j <= i; j++)
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
 
     return dp[n];
 }
