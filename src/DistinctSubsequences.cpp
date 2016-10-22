ddd599a6dff850c0e05b0d6766e29a3f0b505420
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-22 10:46:59 +0800

    Refine Problem 115. Distinct Subsequences

--- src/DistinctSubsequences.cpp
+++ src/DistinctSubsequences.cpp
@@ -10,27 +10,23 @@ int DistinctSubsequences::numDistinct(string s, string t) {
 
     if (ns == 0 || nt == 0) return 0;
 
-    vector<vector<int>> dp(ns, vector<int>(nt, 0));
-    int k = 0;
+    vector<vector<int>> dp(ns + 1, vector<int>(nt + 1, 0));
 
+    int count = 0;
     for (int i = 0; i < ns; i++) {
-        if (t[0] == s[i])
-            k++;
-
-        dp[i][0] = k;
+        if (s[i] == t[0])
+            count++;
+        dp[i + 1][1] = count;
     }
 
-    for (int j = 1; j < nt; j++)
-        dp[0][j] = 0;
-
-    for (int i = 1; i < ns; i++) {
+    for (int i = 0; i < ns; i++) {
         for (int j = 1; j < min(i + 1, nt); j++) {
             if (s[i] == t[j])
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                dp[i + 1][j + 1] = dp[i][j + 1] + dp[i][j];
             else
-                dp[i][j] = dp[i - 1][j];
+                dp[i + 1][j + 1] = dp[i][j + 1];
         }
     }
 
-    return dp[ns - 1][nt - 1];
+    return dp[ns][nt];
 }
