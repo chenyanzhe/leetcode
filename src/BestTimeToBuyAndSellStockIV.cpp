daf495184648a1b313884054f03cb5a4933e99ef
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-22 15:07:04 +0800

    Refine Problem 188. Best Time to Buy and Sell Stock IV

--- src/BestTimeToBuyAndSellStockIV.cpp
+++ src/BestTimeToBuyAndSellStockIV.cpp
@@ -1,5 +1,7 @@
 #include "BestTimeToBuyAndSellStockIV.hpp"
 
+#include <algorithm>
+
 using namespace std;
 
 int BestTimeToBuyAndSellStockIV::maxProfit(int k, vector<int> &prices) {
@@ -9,11 +11,11 @@ int BestTimeToBuyAndSellStockIV::maxProfit(int k, vector<int> &prices) {
         return 0;
 
     if (k >= n / 2) {
-        // quick solve path
+        // In this case, we can complete as many transactions as we like
+        // See also: Best Time to Buy and Sell Stock II
         int ret = 0;
 
         for (int i = 1; i < n; i++) {
-            // as long as there is a price gap, we gain a profit
             if (prices[i] > prices[i - 1])
                 ret += prices[i] - prices[i - 1];
         }
@@ -21,16 +23,15 @@ int BestTimeToBuyAndSellStockIV::maxProfit(int k, vector<int> &prices) {
         return ret;
     }
 
-    vector<vector<int>> dp(k + 1, vector<int>(n, 0));
-
-    for (int i = 1; i <= k; i++) {
-        int maxV = dp[i - 1][0] - prices[0];
-
-        for (int j = 1; j < n; j++) {
-            dp[i][j] = max(dp[i][j - 1], prices[j] + maxV);
-            maxV = max(maxV, dp[i - 1][j] - prices[j]);
+    vector<vector<int>> local(n, vector<int>(k + 1, 0));
+    vector<vector<int>> global(n, vector<int>(k + 1, 0));
+    for (int i = 1; i < n; i++) {
+        int diff = prices[i] - prices[i - 1];
+        for (int j = 1; j <= k; j++) {
+            local[i][j] = max(global[i - 1][j - 1] + max(diff, 0), local[i - 1][j] + diff);
+            global[i][j] = max(global[i - 1][j], local[i][j]);
         }
     }
 
-    return dp[k][n - 1];
+    return global[n - 1][k];
 }
