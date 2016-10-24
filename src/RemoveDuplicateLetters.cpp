b0fd3afb2632a9d0c1f56076d6f259e8b10b1dda
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-24 14:14:09 +0800

    Refine Problem 316. Remove Duplicate Letters

--- src/RemoveDuplicateLetters.cpp
+++ src/RemoveDuplicateLetters.cpp
@@ -1,40 +1,29 @@
 #include "RemoveDuplicateLetters.hpp"
 
-#include <stack>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 string RemoveDuplicateLetters::removeDuplicateLetters(string s) {
-    vector<int> freqs(256, 0);
+    vector<int> freq(256, 0);
     vector<bool> visited(256, false);
-    stack<char> rec;
     string ret;
 
-    for (auto c : s)
-        freqs[c]++;
+    for (auto c : s) freq[c]++;
 
     for (auto c : s) {
-        freqs[c]--;
+        freq[c]--;
 
-        if (visited[c])
-            continue;
+        if (visited[c]) continue;
 
-        while (!rec.empty() && rec.top() > c && freqs[rec.top()]) {
-            visited[rec.top()] = false;
-            rec.pop();
+        while (!ret.empty() && ret.back() > c && freq[ret.back()]) {
+            visited[ret.back()] = false;
+            ret.pop_back();
         }
 
-        rec.push(c);
+        ret += c;
         visited[c] = true;
     }
 
-    while (!rec.empty()) {
-        ret.append(1, rec.top());
-        rec.pop();
-    }
-
-    reverse(ret.begin(), ret.end());
     return ret;
 }
