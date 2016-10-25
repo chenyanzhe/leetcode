d294c32a46c79aaaf8f57e364a830bc8722b2bd8
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-25 20:07:01 +0800

    Refine Problem 234. Palindrome Linked List

--- src/PalindromeLinkedList.cpp
+++ src/PalindromeLinkedList.cpp
@@ -1,65 +1,23 @@
 #include "PalindromeLinkedList.hpp"
 
 bool PalindromeLinkedList::isPalindrome(ListNode *head) {
-    ListNode *p = head;
-    int len = 0;
-
-    while (p != nullptr) {
-        len++;
-        p = p->next;
+    if (!head || !head->next) return true;
+    ListNode *slow = head, *fast = head;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
     }
-
-    if (len == 0 || len == 1)
-        return true;
-
-    if (len == 2)
-        return head->val == head->next->val;
-
-    ListNode *prev = nullptr, *l, *r;
-
-    if (len % 2 == 1) {
-        p = head;
-
-        for (int i = 0; i <= len / 2; i++) {
-            ListNode *next = p->next;
-            p->next = prev;
-            prev = p;
-            p = next;
-        }
-
-        l = prev->next;
-        r = p;
-
-        while (l != nullptr) {
-            if (l->val != r->val)
-                return false;
-
-            l = l->next;
-            r = r->next;
-        }
-
-        return true;
-    } else {
-        p = head;
-
-        for (int i = 0; i < len / 2; i++) {
-            ListNode *next = p->next;
-            p->next = prev;
-            prev = p;
-            p = next;
-        }
-
-        ListNode *l = prev;
-        ListNode *r = p;
-
-        while (l != nullptr) {
-            if (l->val != r->val)
-                return false;
-
-            l = l->next;
-            r = r->next;
-        }
-
-        return true;
+    ListNode *last = slow->next, *pre = head;
+    while (last->next) {
+        ListNode *tmp = last->next;
+        last->next = tmp->next;
+        tmp->next = slow->next;
+        slow->next = tmp;
     }
+    while (slow->next) {
+        slow = slow->next;
+        if (pre->val != slow->val) return false;
+        pre = pre->next;
+    }
+    return true;
 }
