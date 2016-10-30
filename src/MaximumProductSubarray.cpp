31497670d84059bc8aa5037e9670483fa0b55531
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-30 17:47:00 +0800

    Refine Problem 152. Maximum Product Subarray

--- src/MaximumProductSubarray.cpp
+++ src/MaximumProductSubarray.cpp
@@ -5,27 +5,19 @@
 using namespace std;
 
 int MaximumProductSubarray::maxProduct(vector<int> &nums) {
-    int n = nums.size();
+    if (nums.empty()) return 0;
 
-    if (n == 0)
-        return 0;
-
-    vector<int> a(n, 0);
-    vector<int> b(n, 0);
-    a[0] = b[0] = nums[0];
-
-    for (int i = 1; i < n; i++) {
-        int v1 = nums[i];
-        int v2 = a[i - 1] * nums[i];
-        int v3 = b[i - 1] * nums[i];
-        a[i] = max(v1, max(v2, v3));
-        b[i] = min(v1, min(v2, v3));
-    }
-
+    vector<int> a(nums.begin(), nums.end());
+    vector<int> b(nums.begin(), nums.end());
     int ret = a[0];
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < nums.size(); i++) {
+        int v1 = a[i - 1] * nums[i];
+        int v2 = b[i - 1] * nums[i];
+        a[i] = max(a[i], max(v1, v2));
+        b[i] = min(b[i], min(v1, v2));
         ret = max(ret, a[i]);
+    }
 
     return ret;
 }
