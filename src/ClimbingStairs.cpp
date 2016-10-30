871d864fb83f107a9df3da9e103843ca0d534feb
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-30 16:54:01 +0800

    Refine Problem 70. Climbing Stairs

--- src/ClimbingStairs.cpp
+++ src/ClimbingStairs.cpp
@@ -1,17 +1,18 @@
 #include "ClimbingStairs.hpp"
 
+#include <vector>
+
+using namespace std;
+
 int ClimbingStairs::climbStairs(int n) {
     if (n == 0 || n == 1 || n == 2)
         return n;
 
-    int a = 1;
-    int b = 2;
+    vector<int> dp(n + 1, 0);
+    dp[0] = 0, dp[1] = 1, dp[2] = 2;
 
-    for (int i = 2; i < n; i++) {
-        int c = a + b;
-        a = b;
-        b = c;
-    }
+    for (int i = 3; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
 
-    return b;
+    return dp[n];
 }
