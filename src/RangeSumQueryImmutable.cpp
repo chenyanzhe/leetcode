41730f585531d70b6727400ccbc19eb64ac6ddcd
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-31 10:44:04 +0800

    Refine Problem 303. Range Sum Query - Immutable

--- src/RangeSumQueryImmutable.cpp
+++ src/RangeSumQueryImmutable.cpp
@@ -1,14 +1,8 @@
 #include "RangeSumQueryImmutable.hpp"
 
-NumArray::NumArray(vector<int> &nums) {
-    if (nums.empty())
-        return;
-
-    accs.resize(nums.size());
-    accs[0] = nums[0];
-
+NumArray::NumArray(vector<int> &nums) : accs(nums.begin(), nums.end()) {
     for (int i = 1; i < nums.size(); i++)
-        accs[i] = accs[i - 1] + nums[i];
+        accs[i] += accs[i - 1];
 }
 
 int NumArray::sumRange(int i, int j) {
