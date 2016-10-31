6a819b4390249d2a909ca72915cf1e0a79fd5151
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-31 11:13:07 +0800

    Refine Problem 322. Coin Change

--- src/CoinChange.cpp
+++ src/CoinChange.cpp
@@ -11,12 +11,10 @@ int CoinChange::coinChange(vector<int> &coins, int amount) {
     vector<int> dp(amount + 1, INT_MAX);
     dp[0] = 0;
 
-    for (auto c : coins) {
-        for (auto i = c; i <= amount; i++) {
-            if (dp[i - c] != INT_MAX)
-                dp[i] = min(dp[i - c] + 1, dp[i]);
-        }
-    }
+    for (int i = 1; i <= amount; i++)
+        for (auto c : coins)
+            if (c <= i && dp[i - c] < INT_MAX)
+                dp[i] = min(dp[i], dp[i - c] + 1);
 
     return dp[amount] == INT_MAX ? -1 : dp[amount];
 }
--- tests/CoinChangeTest.cpp
+++ tests/CoinChangeTest.cpp
@@ -4,9 +4,13 @@
 
 TEST_CASE("Coin Change") {
     CoinChange s;
-    SECTION("Sample test") {
-        vector<int> coins{1, 2, 5};
-        int amount = 11;
-        REQUIRE(s.coinChange(coins, amount) == 3);
+    SECTION("Sample tests") {
+        vector<int> coins1{1, 2, 5};
+        int amount1 = 11;
+        REQUIRE(s.coinChange(coins1, amount1) == 3);
+
+        vector<int> coins2{2};
+        int amount2 = 3;
+        REQUIRE(s.coinChange(coins2, amount2) == -1);
     }
 }
