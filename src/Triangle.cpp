28481efaa0c80a28be9e86df3e44d4d33ef50350
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-31 10:15:31 +0800

    Refine Problem 120. Triangle

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -3,17 +3,29 @@
 int Triangle::minimumTotal(vector<vector<int>> &triangle) {
     if (triangle.empty()) return 0;
 
-    int n = triangle.size();
-    vector<int> dp(n, 0);
+    size_t n = triangle.size();
 
-    for (int i = 0; i < n; i++)
-        dp[i] = triangle[n - 1][i];
+    if (triangle[n - 1].size() != n) return 0;
 
-    for (int i = n - 2; i >= 0; i--) {
-        for (int j = 0; j <= i; j++) {
-            dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    dp[0][0] = triangle[0][0];
+
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = dp[i - 1][0] + triangle[i][0];
+        dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < i; j++) {
+            dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
         }
     }
 
-    return dp[0];
+    int ret = INT_MAX;
+
+    for (int j = 0; j < n; j++)
+        ret = min(ret, dp[n - 1][j]);
+
+    return ret;
 }
