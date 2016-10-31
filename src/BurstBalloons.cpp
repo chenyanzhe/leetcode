a877fa9cf301090f66abc5223db6012ac0a679a6
Yanzhe Chen <yanzhe.cn@gmail.com>
2016-10-31 10:34:29 +0800

    Refine Problem 312. Burst Balloons

--- src/BurstBalloons.cpp
+++ src/BurstBalloons.cpp
@@ -3,23 +3,21 @@
 int BurstBalloons::maxCoins(vector<int> &nums) {
     if (nums.empty()) return 0;
 
-    int n = nums.size();
+    nums.insert(nums.begin(), 1);
+    nums.push_back(1);
 
-    vector<int> exnums(n + 2, 0);
+    size_t n = nums.size();
 
-    exnums[0] = exnums[n + 1] = 1;
-    for (int i = 1; i <= n; i++)
-        exnums[i] = nums[i - 1];
+    vector<vector<int>> dp(n, vector<int>(n, 0));
 
-    vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
-
-    for (int k = 2; k < n + 2; k++) {
-        for (int l = 0; l + k < n + 2; l++) {
-            int r = l + k;
-            for (int m = l + 1; m < r; m++)
-                dp[l][r] = max(dp[l][r], exnums[l] * exnums[m] * exnums[r] + dp[l][m] + dp[m][r]);
+    for (int len = 2; len < n; len++) {
+        for (int i = 0; i + len < n; i++) {
+            int j = i + len;
+            for (int m = i + 1; m < j; m++) {
+                dp[i][j] = max(dp[i][j], dp[i][m] + dp[m][j] + nums[i] * nums[m] * nums[j]);
+            }
         }
     }
 
-    return dp[0][n + 1];
+    return dp[0][n - 1];
 }
